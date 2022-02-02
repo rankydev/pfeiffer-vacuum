b8c37a199f229e91d53240c22050f6b16ae87894
philipp.timmel <philipp.timmel@diva-e.com>
2022-02-02 11:44:40 +0100

    PVWEB-383 | fix | fixed tailwind and typography configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,3 @@
-const plugin = require('tailwindcss/plugin')
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   important: false,
   content: {
@@ -14,34 +11,29 @@ module.exports = {
     ],
   },
   theme: {
-    extend: {
-      colors: {
-        'pv-red': '#dd1541',
-        'pv-red-darker': '#c01238',
-        'pv-red-lighter': '#e13258',
-        'pv-yellow': '#f2e600',
-        'pv-yellow-darker': '#e6d200',
-        'pv-yellow-lighter': '#f9f300',
-        'pv-green': '#66cc77',
-        'pv-warning': '#ffa64d',
-        'pv-grey-16': '#292929',
-        'pv-grey-32': '#515151',
-        'pv-grey-48': '#7a7a7a',
-        'pv-grey-64': '#a3a3a3',
-        'pv-grey-80': '#cccccc',
-        'pv-grey-96': '#f5f5f5',
-        green: colors.emerald,
-        yellow: colors.amber,
-        purple: colors.violet,
-      },
-      fontFamily: {
-        sans: ['UniversLTStd', 'Helvetica Neue'],
-      },
-      gridTemplateColumns: {
-        desktop: 'repeat(12, minmax(0, 1fr))',
-        tablet: 'repeat(8, minmax(0, 1fr))',
-        mobile: 'repeat(4, minmax(0, 1fr))',
-      },
+    colors: {
+      'pv-red': '#dd1541',
+      'pv-red-darker': '#c01238',
+      'pv-red-lighter': '#e13258',
+      'pv-yellow': '#f2e600',
+      'pv-yellow-darker': '#e6d200',
+      'pv-yellow-lighter': '#f9f300',
+      'pv-green': '#66cc77',
+      'pv-warning': '#ffa64d',
+      'pv-grey-16': '#292929',
+      'pv-grey-32': '#515151',
+      'pv-grey-48': '#7a7a7a',
+      'pv-grey-64': '#a3a3a3',
+      'pv-grey-80': '#cccccc',
+      'pv-grey-96': '#f5f5f5',
+    },
+    fontFamily: {
+      sans: ['UniversLTStd', 'Helvetica Neue'],
+    },
+    gridTemplateColumns: {
+      desktop: 'repeat(12, minmax(0, 1fr))',
+      tablet: 'repeat(8, minmax(0, 1fr))',
+      mobile: 'repeat(4, minmax(0, 1fr))',
     },
     screens: {
       sm: '0px',
@@ -61,10 +53,6 @@ module.exports = {
         xl: '1280px',
       },
     },
-    fontFamily: {
-      sans: ['UniversLTStd', 'Helvetica Neue'],
-    },
   },
-  plugins: [require('@tailwindcss/forms')],
   prefix: 'tw-',
 }
