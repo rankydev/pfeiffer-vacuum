8ffa09a8379fa26424c35d4c36cc227a94905d73
Daniela Hofmann <daniela.hofmann@diva-e.com>
2022-02-11 08:13:07 +0100

    PVWEB-405 | feat | add ContentWrapper.vue

---
+++ components/molecules/ContentWrapper/ContentWrapper.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="content-wrapper">
+    <NuxtDynamic
+      v-for="item in content.items"
+      :key="item._uid"
+      v-editable="true"
+      :name="item.component"
+      :content="item"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContentWrapper',
+  props: {
+    content: {
+      type: Object,
+      default: () => {},
+      required: true,
+    },
+  },
+}
+</script>
