ab6fe5af80187ca707fb41b5e90232f0b4a4ced4
Manuel Bolai <manuel.bolai@diva-e.com>
2022-03-15 14:07:16 +0100

    PVWEB-349 | chore | merge develop into branch and disable richtext temp

--- components/atoms/Richtext/Richtext.vue
+++ components/atoms/Richtext/Richtext.vue
@@ -1,6 +1,12 @@
 <!-- eslint-disable vue/no-v-html -->
 <template>
-  <div v-editable="content" class="paragraph-section" v-html="richtext"></div>
+  <div>richtext</div>
+  <!--
+  <div v-editable="content"
+       class="paragraph-section"
+       v-html="richtext">
+       </div>
+       -->
 </template>
 <script>
 import { computed, defineComponent } from '@nuxtjs/composition-api'
@@ -13,7 +19,7 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const richtext = computed(() => props.content.richtext)
+    const richtext = computed(() => props?.content?.richtext)
 
     return { richtext }
   },
