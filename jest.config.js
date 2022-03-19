18cc8c37c8f26a46c114bf708ece65423ccf0162
Veit Bjarsch <vb@poweruplink.com>
2022-03-19 02:24:27 +0100

    SUPPVAC-111 | fix | test with latest modules

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,10 @@ module.exports = {
   moduleFileExtensions: ['js', 'vue', 'json'],
   transform: {
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest',
+    '.*\\.(vue)$': '@vue/vue2-jest',
   },
   collectCoverage: true,
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   coverageReporters: ['clover', 'json', 'lcov', 'text', 'html'],
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
