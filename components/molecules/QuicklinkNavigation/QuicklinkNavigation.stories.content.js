4cf80382dac7dfc80ac23d900c8b6efb39313c9a
Veit Bjarsch <vb@poweruplink.com>
2022-03-23 08:05:59 +0100

    PVWEB-318 | fix | fixed content

--- components/molecules/QuicklinkNavigation/QuicklinkNavigation.stories.content.js
+++ components/molecules/QuicklinkNavigation/QuicklinkNavigation.stories.content.js
@@ -1,24 +1,11 @@
-export const quicklinks = [
-  {
-    _uid: '164e89d0-5d6b-4d97-a66a-afc6f39305d1',
-    label: 'Overview',
-    anchor: 'testAnchor1',
-  },
-  {
-    _uid: '164e89d0-5d6b-4d97-a66a-afc6f39305d2',
-    label: 'Markets',
-    anchor: 'testAnchor2',
-  },
-  {
-    _uid: '164e89d0-5d6b-4d97-a66a-afc6f39305d3',
-    label: 'Frequently searched',
-    anchor: 'testAnchor3',
-  },
-  {
-    _uid: '164e89d0-5d6b-4d97-a66a-afc6f39305d4',
-    label: 'News',
-    anchor: 'testAnchor4',
-  },
-]
+import { quicklinks } from '~/components/molecules/Links/Quicklink/QuickLink.stories.content.js'
+
+export const quicklinkNavigation = {
+  _uid: '83765e37-cc94-4ecb-84fa-f25b57dbb4de',
+  component: 'OnPageNavigation',
+  quicklinks,
+  _editable:
+    '<!--#storyblok#{"name": "OnPageNavigation", "space": "144293", "uid": "83765e37-cc94-4ecb-84fa-f25b57dbb4de", "id": "104316822"}-->',
+}
 
 export default {}
