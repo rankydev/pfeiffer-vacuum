d900da04d73e800e118b9faa04c0affdabcaa3b6
Veit Bjarsch <vb@poweruplink.com>
2022-04-14 05:51:18 +0200

    PVWEN-322 | feat | small navigation improvements

--- components/organisms/Header/partials/MainNavigationLevel/MainNavLinkMobile.schema.js
+++ components/organisms/Header/partials/MainNavigationLevel/MainNavLinkMobile.schema.js
@@ -1,14 +1,13 @@
 import Link from '~/components/atoms/Link/Link.schema.js'
 
 export default {
-  name: 'MainNavLinkLevel2',
-  display_name: 'Navigation Entry',
+  name: 'MainNavLinkMobile',
+  display_name: 'Mobile Navigation Entry',
   schema: {
     label: {
       type: 'text',
       translatable: true,
       display_name: 'Label',
-      required: true,
     },
     ...Link.schema,
   },
@@ -18,6 +17,6 @@ export default {
   is_nestable: true,
   all_presets: [],
   preset_id: null,
-  real_name: 'MainNavLinkLevel2',
+  real_name: 'MainNavLinkMobile',
   component_group_name: 'Global',
 }
--- components/organisms/Header/partials/MainNavigationLevel/MainNavigationLevel.vue
+++ components/organisms/Header/partials/MainNavigationLevel/MainNavigationLevel.vue
@@ -1,90 +1,99 @@
 <template>
-  <ul
-    :class="{
-      [`primary-nav-${level}`]: true,
-      [`primary-nav-${level}--passive`]: activeElement !== null,
-    }"
-    @mouseenter="isHovered = true"
-    @mouseleave="isHovered = false"
-  >
-    <li
-      v-for="(entry, idx) in enrichedEntries"
-      :key="idx"
-      :class="[`primary-nav-${level}__element`, entry.class || '']"
+  <div :class="`primary-nav-${level}__wrapper`">
+    <ul
+      :class="{
+        [`primary-nav-${level}`]: true,
+        [`primary-nav-${level}--passive`]: hasActiveElement,
+      }"
+      @mouseenter="isHovered = true"
+      @mouseleave="isHovered = false"
     >
-      <Link
-        :class="{
-          [`primary-nav-${level}__link`]: true,
-          [`primary-nav-${level}__link--inactive`]: ![null, idx].includes(
-            activeElement
-          ),
-          [`primary-nav-${level}__link--active`]: activeElement === idx,
-        }"
-        v-bind="entry"
-        :before-navigation="($event) => toggleActive($event, idx)"
-      >
-        <span :class="`primary-nav-${level}__label`">{{ entry.label }}</span>
-        <Icon
-          v-if="(entry.navigationEntries || []).length > 0"
-          :class="`primary-nav-${level}__icon`"
-          :icon="activeElement === idx ? 'expand_less' : 'expand_more'"
-          @click.native="toggleActive($event, idx)"
-          @keypress.native.enter="toggleActive($event, idx)"
-        />
-      </Link>
-      <Component
-        :is="level === 0 ? 'AnimatedCollapse' : 'div'"
-        v-if="(entry.navigationEntries || []).length > 0"
-        speed="fast"
-      >
-        <div
-          v-if="activeElement === idx"
-          :class="`primary-nav-${level}__sub-nav`"
-          @mouseenter="isHovered = false"
-          @mouseleave="isHovered = true"
-        >
-          <MainNavigationLevel
-            :current-entry="entry"
-            :level="level + 1"
-            :navigation-entries="entry.navigationEntries"
-            @show-buttons="(value) => (showButtons = value)"
-          />
-        </div>
-      </Component>
-    </li>
-    <template v-if="level > 0">
-      <li v-if="isMobile && activeElement == null">
-        <Link
-          :href="currentEntry.href || ''"
-          :class="[`primary-nav-${level}__link`]"
-        >
-          <span :class="`primary-nav-${level}__label`">All Products</span>
-          <Icon :class="`primary-nav-${level}__icon`" icon="arrow_forward" />
-        </Link>
-      </li>
       <li
-        v-if="
-          isMobile
-            ? activeElement == null
-            : isHovered || (activeElement == null && !$parent.isHovered)
-        "
+        v-for="(entry, idx) in navigationEntries"
+        :key="idx"
         :class="{
-          [`primary-nav-${level}__buttons`]: true,
+          [`primary-nav-${level}__element`]: true,
+          ['md:tw-hidden']: entry.component === 'MainNavLinkMobile',
         }"
       >
-        <Button
-          v-if="!isMobile"
-          class="tw-hidden md:tw-block"
-          variant="secondary"
-          shape="outlined"
-          size="small"
-          label="Übersicht"
-          :href="currentEntry.href"
-        />
-        <Button size="small" label="Shop" :href="currentEntry.shopLink" />
+        <Link
+          :class="{
+            [`primary-nav-${level}__link`]: true,
+            [`primary-nav-${level}__link--passive`]: ![null, idx].includes(
+              activeElement
+            ),
+            [`primary-nav-${level}__link--active`]: activeElement === idx,
+          }"
+          v-bind="entry"
+          :before-navigation="
+            ($e) => !hasSubmenu(entry) || toggleActive($e, idx)
+          "
+        >
+          <span :class="`primary-nav-${level}__label`">{{ entry.label }}</span>
+          <Icon
+            v-if="hasSubmenu(entry)"
+            :class="`primary-nav-${level}__icon`"
+            :icon="activeElement === idx ? 'expand_less' : 'expand_more'"
+            @click.native="toggleActive($event, idx)"
+            @keypress.native.enter="toggleActive($event, idx)"
+          />
+        </Link>
+        <client-only>
+          <Component
+            :is="isMobile || level === 0 ? 'AnimatedCollapse' : 'div'"
+            v-if="hasSubmenu(entry)"
+            speed="medium"
+          >
+            <MainNavigationLevel
+              v-if="activeElement === idx"
+              :current-entry="entry"
+              :level="level + 1"
+              :navigation-entries="entry.navigationEntries"
+              @mouseenter.native="isHovered = false"
+              @mouseleave.native="isHovered = true"
+            />
+          </Component>
+        </client-only>
       </li>
-    </template>
-  </ul>
+
+      <template v-if="level > 0">
+        <li v-if="isMobile && !hasActiveElement">
+          <Link
+            :href="currentEntry.href || ''"
+            :class="[`primary-nav-${level}__link`]"
+          >
+            <span :class="`primary-nav-${level}__label`">All Products</span>
+            <Icon :class="`primary-nav-${level}__icon`" icon="arrow_forward" />
+          </Link>
+        </li>
+
+        <li
+          v-if="
+            isMobile
+              ? !hasActiveElement && currentEntry.shopLink
+              : isHovered || (!hasActiveElement && !$parent.isHovered)
+          "
+          :class="`primary-nav-${level}__buttons`"
+        >
+          <Button
+            v-if="!isMobile"
+            class="tw-hidden md:tw-block"
+            variant="secondary"
+            shape="outlined"
+            size="small"
+            label="Overview"
+            :href="currentEntry.href"
+          />
+          <Button
+            v-if="currentEntry.shopLink"
+            size="small"
+            label="Shop"
+            :href="currentEntry.shopLink"
+          />
+        </li>
+      </template>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -122,12 +131,13 @@ export default defineComponent({
       default: 0,
     },
   },
-  setup(props, { root }) {
+  setup(props) {
     const { app } = useContext()
 
     const menu = useMenuStore()
 
     const activeElement = ref(null)
+    const hasActiveElement = computed(() => activeElement.value !== null)
     const isMobile = app.$breakpoints.isMobile
     const isHovered = ref(false)
 
@@ -152,19 +162,15 @@ export default defineComponent({
       return false
     }
 
-    const home = { label: 'Home', href: '/', class: 'md:tw-hidden' }
-    const enrichedEntries = computed(() => {
-      return props.level === 0
-        ? [home, ...props.navigationEntries]
-        : props.navigationEntries
-    })
+    const hasSubmenu = (entry) => entry?.navigationEntries?.length > 0
 
     return {
       toggleActive,
+      hasActiveElement,
       activeElement,
       isMobile,
       isHovered,
-      enrichedEntries,
+      hasSubmenu,
     }
   },
 })
