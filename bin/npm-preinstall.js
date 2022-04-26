c5fd4e898573fc891d8151f6a11be4588935685a
Veit Bjarsch <vb@poweruplink.com>
2022-04-26 02:14:28 +0200

    PVWEB-0815 | chore | added package version check

---
+++ bin/npm-preinstall.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node -r esm
+
+import { lockfileVersion } from '../package-lock.json'
+import { engines } from '../package.json'
+
+if (lockfileVersion !== 2) {
+  console.error('Your lockfile version is outdated')
+  console.error(`Please use node ${engines.node} with npm ${engines.npm}`)
+  console.error(
+    `Please run 'git reset ./package-lock.json' and 'npm i' with the appropriate node version`
+  )
+  process.exit(1)
+}
