9791794964b6bd3157c054ea35361341c851d3a6
Philipp Bender <philipp.bender@diva-e.com>
2022-04-27 11:48:44 +0200

    Cleanup custom link resolver

--- resolver/customLinkResolver.js
+++ resolver/customLinkResolver.js
@@ -1,11 +1,3 @@
-const isEmailLinkType = type => type === 'email'
-const isStoryLinkType = type => type === 'story'
-const isBrowser = () => process.client && window
-const getQueryParams = () => isBrowser() ? window.location.search : ''
-const isPreview = () => getQueryParams().includes('_storyblok')
-
-
-export function render ({node, $storyblok, links, lang}) {
-  console.log('ENTER link resolver')
+export function render({ node, $storyblok, links, lang }) {
   return 'https://www.pfeiffer-vacuum.com/de/'
 }
