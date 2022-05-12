f4c2a624552a896ce75e47c6cacc69f972b12f12
Jessica Holst <>
2022-05-12 16:52:40 +0200

    PVWEB-325 | feat | added Test for testing content but didnt work

--- components/organisms/HomeStageModule/partials/HomeStageContent/HomeStageContent.spec.js
+++ components/organisms/HomeStageModule/partials/HomeStageContent/HomeStageContent.spec.js
@@ -1,21 +1,21 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils'
-import HomeStageContent from './HomeStageContent'
-// import HomeStageContent from '~/components/organisms/HomeStageModule/partials/HomeStageContent/HomeStageContent'
+// import HomeStageContent from './HomeStageContent'
+import HomeStageContent from '~/components/organisms/HomeStageModule/partials/HomeStageContent/HomeStageContent'
 import content from './HomeStageContent.stories.content'
+
+// import content from './HomeStageModule.stories.content'
 
 const defaultProps = () => JSON.parse(JSON.stringify(content))
 
 let wrapper
 
 function createComponent(propsData = {}) {
-  const stubs = { NuxtImg: true }
   const localVue = createLocalVue()
   const editable = (el, key) => (el.innerText = key.value)
   localVue.directive('editable', editable)
 
   const options = {
     localVue,
-    stubs,
     propsData,
   }
 
@@ -29,5 +29,31 @@ describe('HomeStageContent', () => {
       const homeStageContentWrapper = wrapper.find('.homestage-content')
       expect(homeStageContentWrapper.exists()).toBeTruthy()
     })
+
+    // headline
+    test('should render headline', () => {
+      const propsData = {
+        ...defaultProps(),
+      }
+      createComponent(propsData)
+      const homeStageContentWrapper = wrapper.find('.homestage-content')
+      console.log(homeStageContentWrapper)
+      // // console.log(homeStageContentWrapper.at(0).attributes('headline'))
+      // // console.log(homeStageContentWrapper.vm)
+      // // console.log(homeStageContentWrapper.attributes('headline'))
+      // // console.log(propsData[0].headline)
+      // const headline = propsData[0].headline
+      // const headlineWrapper = homeStageContentWrapper.find(
+      //   '.homestage-content__headline'
+      // )
+      // console.log(headlineWrapper)
+      // expect(headlineWrapper.exists()).toBeTruthy()
+      // console.log(homeStageContentWrapper)
+      // expect(homeStageContentWrapper.exists()).toBeTruthy()
+    })
+
+    // teaserText
+
+    // position
   })
 })
