107749d9a80440d342e9905805d6ae7e6f954a43
Daniela Hofmann <daniela.hofmann@diva-e.com>
2022-05-13 15:51:18 +0200

    PVWEB-394 | feat | change example content according to new data structure

--- components/atoms/Carousel/Carousel.stories.content.js
+++ components/atoms/Carousel/Carousel.stories.content.js
@@ -1,15 +1,17 @@
 import {
-  productCardCategory,
-  productCardTitle,
-  productCardDescription,
-  productCardAdditionalInformation,
-} from '~/components/atoms/Card/Card.stories.content'
+  targets,
+  href,
+  target,
+  image,
+  headline,
+} from '~/components/molecules/TeaserCard/TeaserCard.stories.content'
 
 export const carouselEntry = {
-  productCardCategory,
-  productCardTitle,
-  productCardDescription,
-  productCardAdditionalInformation,
+  targets,
+  href,
+  target,
+  image,
+  headline,
 }
 
 export const carouselEntries = [
@@ -22,4 +24,24 @@ export const carouselEntries = [
   carouselEntry,
 ]
 
-export default {}
+export default {
+  autoplay: false,
+  button: [
+    {
+      _uid: '5f3551f0-2e8e-4ddb-a7ed-938b08be3f4c',
+      icon: 'arrow_forward',
+      size: 'normal',
+      label: 'Btn',
+      shape: 'plain',
+      variant: 'secondary',
+      disabled: false,
+      component: 'Button',
+      prependIcon: false,
+    },
+  ],
+  headline: 'Headline',
+  infinite: true,
+  isWide: true,
+  settings: {},
+  slides: carouselEntries,
+}
