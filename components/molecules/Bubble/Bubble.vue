99db203402e4b55b91a7b3136b1a56d48ee703ea
julius.schultz <julius.schultz@diva-e.com>
2022-05-16 14:38:42 +0200

    PVWEB-325 | feat |  rebuild stories to include new richtext component.

--- components/molecules/Bubble/Bubble.vue
+++ components/molecules/Bubble/Bubble.vue
@@ -31,7 +31,7 @@ export default defineComponent({
       default: '',
     },
     /**
-     * richtext element
+     * richtext element, containing the description
      */
     richtext: {
       type: Array,
