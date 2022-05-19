c452d33ac662cc0eb9e194b41c112b6b75a0237f
julius.schultz <julius.schultz@diva-e.com>
2022-05-19 11:53:23 +0200

    PVWEB-336 | feat | created .schema and .vue file

---
+++ components/organisms/ProductCard/ProductCard.schema.js
@@ -0,0 +1,20 @@
+export default {
+  name: 'ProductCard',
+  display_name: 'ProductCard',
+  schema: {
+    productID: {
+      type: 'text',
+      required: true,
+      pos: 1,
+      max: 1,
+    },
+  },
+  image: null,
+  preview_field: null,
+  is_root: false,
+  preview_tmpl: null,
+  is_nestable: true,
+  all_presets: [],
+  preset_id: null,
+  real_name: 'ProductCard',
+}
---
+++ components/organisms/ProductCard/ProductCard.vue
@@ -0,0 +1,21 @@
+<template></template>
+
+<script>
+export default defineComponent({
+  components: {},
+  props: {
+    /**
+     * Headline to display the teaser
+     */
+    productID: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Image data for responsive image component
+     */
+  },
+})
+</script>
+
+<style lang="scss"></style>
