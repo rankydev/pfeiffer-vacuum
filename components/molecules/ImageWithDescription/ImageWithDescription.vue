fe4b93554d8004f16256b0c6af2970c328d92ef1
Jessica Holst <>
2022-05-19 11:01:31 +0200

    PVWEB-331 | fix | made descriptio text color grey and correct margin-top of 8px

--- components/molecules/ImageWithDescription/ImageWithDescription.vue
+++ components/molecules/ImageWithDescription/ImageWithDescription.vue
@@ -48,8 +48,16 @@ export default defineComponent({
     aspectRatio: {
       type: String,
       default: '1:1',
-      validator: (val) => ['1:1', '16:9', '2:3', '3:2', '3:1'].includes(val),
+      // validator: (val) => ['1:1', '16:9', '2:3', '3:2', '3:1'].includes(val),
     },
   },
 })
 </script>
+<style lang="scss">
+.image-with-description {
+  &__component {
+    @apply tw-text-pv-grey-48;
+    @apply tw-mt-2;
+  }
+}
+</style>
