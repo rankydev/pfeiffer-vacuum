ed8d6f675372403cf177f745b34c33854bc9b261
julius.schultz <julius.schultz@diva-e.com>
2022-05-25 21:45:46 +0200

    PVWEB-347 | feat | added tests

--- components/organisms/Footer/partials/FooterNewsletter/FooterNewsletter.stories.content.js
+++ components/organisms/Footer/partials/FooterNewsletter/FooterNewsletter.stories.content.js
@@ -1,15 +1,3 @@
-import {
-  bubbleText,
-  smallText,
-} from '~/components/atoms/Richtext/Richtext.stories.content.js'
-import Video from '@/components/atoms/VideoPlayer/VideoPlayer.stories.content'
-import CTABox from '@/components/molecules/CTABox/CTABox.stories.content'
-import { imageWithDescriptionContent } from '@/components/molecules/ImageWithDescription/ImageWithDescription.stories.content'
-import {
-  buttonFilledSecondary,
-  buttonOutlinedSecondary,
-} from '@/components/atoms/Button/Button.stories.content'
-
 export default {
   headline: 'Newsletter',
   richtext: [
