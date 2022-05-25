97d67e7b275d33c69fa803af6030e4865b0be69e
Jessica Holst <>
2022-05-25 09:14:44 +0200

    PVWEB-624 | feature | added tests from Generic Card into Product Card

---
+++ components/organisms/ProductCard/ProductCard.spec.js
@@ -0,0 +1,94 @@
+import { shallowMount, createLocalVue, mount } from '@vue/test-utils'
+import ProductCard from './ProductCard.vue'
+import {
+  imageSizes,
+  href,
+  subheading,
+  heading,
+  description,
+  additionalInfo,
+} from './ProductCard.stories.content'
+
+const defaultProps = () => ({
+  imageSizes,
+  href,
+  subheading,
+  heading,
+  description,
+  additionalInfo,
+})
+
+let wrapper
+
+function createComponent(propsData = {}, { shallow = true, slots = {} } = {}) {
+  const stubs = {}
+  const localVue = createLocalVue()
+  const editable = (el, key) => (el.innerText = key.value)
+  localVue.directive('editable', editable)
+
+  const options = {
+    localVue,
+    stubs,
+    propsData,
+    slots,
+  }
+
+  wrapper = shallow
+    ? shallowMount(ProductCard, options)
+    : mount(ProductCard, options)
+}
+
+describe('ProductCard', () => {
+  describe('initial state', () => {
+    it('should render given the minimal setup', () => {
+      const propsData = { ...defaultProps() }
+      createComponent(propsData)
+
+      expect(wrapper.vm).toBeTruthy()
+    })
+
+    it('should use the image size cover as default', () => {
+      const propsData = { ...defaultProps() }
+      createComponent(propsData)
+
+      const imageWrapper = wrapper.find('.pv-card__image')
+
+      expect(imageWrapper.attributes('class')).toMatch('pv-card__image--cover')
+    })
+
+    it('should use the image size contain given contain', () => {
+      const propsData = { ...defaultProps(), imageSize: 'contain' }
+      createComponent(propsData)
+
+      const imageWrapper = wrapper.find('.pv-card__image')
+
+      expect(imageWrapper.attributes('class')).toMatch(
+        'pv-card__image--contain'
+      )
+    })
+
+    it('should render all content given slot content', () => {
+      const tag = '<div class="test-tag" />'
+      const image = '<div class="test-image" />'
+      const subheading = '<div class="test-subheading" />'
+      const heading = '<div class="test-heading" />'
+      const tags = '<div class="test-tags" />'
+      const description = '<div class="test-description" />'
+      const slots = { tag, image, subheading, heading, tags, description }
+
+      const propsData = { ...defaultProps() }
+      createComponent(propsData, { shallow: false, slots })
+
+      const productCard = wrapper.findComponent(ProductCard)
+
+      expect(productCard.find('.test-tag')).toBeTruthy()
+      expect(productCard.find('.test-image')).toBeTruthy()
+      expect(productCard.find('.test-subheading')).toBeTruthy()
+      expect(productCard.find('.test-heading')).toBeTruthy()
+      expect(productCard.find('.test-tags')).toBeTruthy()
+      expect(productCard.find('.test-description')).toBeTruthy()
+    })
+  })
+
+  // describe('business requirements', () => {})
+})
--- components/organisms/ProductCard/ProductCard.vue
+++ components/organisms/ProductCard/ProductCard.vue
@@ -1,12 +1,7 @@
 <template>
-  <GenericCard v-bind="{ imageSize }">
-    <template #tag></template>
+  <GenericCard :image-size="'contain'">
     <template #image>
-      <ResponsiveImage
-        provider="storyblok"
-        :image="product.images[0]"
-        :aspect-ratio="'2:1'"
-      />
+      <img :src="product.images[0].url" :alt="product.images[0].altText" />
     </template>
     <template #subheading>
       {{ product.categories[0].name }}
@@ -18,18 +13,14 @@
     <template #description>
       <p v-html="product.description" />
     </template>
-    <template #additionalInfo>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus ornare
-        est diam aliquet turpis enim felis bibendum net…
-      </p>
-    </template>
+    <template #additionalInfo><p>Order Number</p></template>
     <template #actionItems></template>
   </GenericCard>
 </template>
 
 <script>
 import { defineComponent } from '@nuxtjs/composition-api'
+import { useProductStore } from '~/stores/products/products.js'
 
 export default defineComponent({
   components: {},
@@ -42,12 +33,14 @@ export default defineComponent({
       default: '',
     },
   },
-  setup() {
+  setup(props) {
+    const productStore = useProductStore
+
     const product = {
       categories: [
         {
           id: 'HIPACE__80',
-          name: 'HiPace® 80',
+          name: 'Product Category',
         },
       ],
       categoryPath: [
@@ -66,16 +59,17 @@ export default defineComponent({
       ],
       code: '128ee16d-cb90-45be-9986-c8006a5235e6_sample',
       configurable: false,
-      description: '',
+      description:
+        ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus ornare est diam aliquet turpis enim felis bibendum net…',
       images: [
         {
           altText: 'HiPace® 80 for TCP 350, DN 63 ISO-K',
           format: 'product',
           imageType: 'PRIMARY',
-          url: '/medias/?context=bWFzdGVyfGltYWdlc3wzNTE0Njd8aW1hZ2UvcG5nfGgzNy9oMjMvODgxOTQ1NzA2NDk5MHw5MjM5ZmNhYWRkODlmYWZlMWY5ODhlODA5NWYwOGM2ODAzZDlkM2FlMzkxYWZkZjhlZjVlNzJmOWM1NjMwNTZm',
+          url: 'https://s3-alpha-sig.figma.com/img/033b/dea9/9f70aca9d3145d447e48fcca9b5c2a9f?Expires=1654473600&Signature=XgK0WlT01RiuSSnK02gmCGT~7d1SfufChxeOZkD8uzmAVBmoMON8ATvyvXPt02EGReClbU~KmaR9-HBs1tDOKasXO3gJjM0EAaAc7xuCyjLCEMnBVBqZNWGsx~YaC3363UXEo92fOmyprnLO2HgxpamyWQrOXYYZGcNL~7ES2JzI3suuC9koJ9-HGZQNFCIZozWKtlA39M89stkBnfg0A2UOBNFjJSzzP7CDT5suYFT1nMw87c8nMsG2jr8x6HbxjYjx7~HJjBavY-iiDt2dIlTGGZu7P5wDnhkGP6sEQU8PFWjNTzDNdUsYRiI~M1k3a2Qi2bTauUfnZ19Zrzr0bA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA',
         },
       ],
-      name: 'HiPace® 80',
+      name: 'Product variant name',
       numberOfVariants: 21,
       priceRange: {},
       productType: 'MASTERPRODUCT',
@@ -87,7 +81,6 @@ export default defineComponent({
       summary: '',
       url: '/Pumps/Turbopumps/HiPace%C2%AE-80/HiPace%C2%AE-80/p/128ee16d-cb90-45be-9986-c8006a5235e6_sample',
     }
-
     return { product }
   },
 })
