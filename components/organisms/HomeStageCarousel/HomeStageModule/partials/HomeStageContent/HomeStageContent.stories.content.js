0f58a9b8764478b674ba72f69f37d6d8c544ca6c
julius.schultz <julius.schultz@diva-e.com>
2022-05-30 16:35:41 +0200

    PVWEB-347 | fix | fix Bubble Tests

--- components/organisms/HomeStageCarousel/HomeStageModule/partials/HomeStageContent/HomeStageContent.stories.content.js
+++ components/organisms/HomeStageCarousel/HomeStageModule/partials/HomeStageContent/HomeStageContent.stories.content.js
@@ -1,41 +1,37 @@
-import { bubbleText } from '@/components/atoms/Richtext/Richtext.stories.content'
-
-export default [
-  {
-    headline: 'Komplettlösungen für jeden Vakuumbereich',
-    component: 'HomeStageContent',
-    richtext: [
-      {
-        _uid: '4d12b093-e25f-4ff2-bc8c-f645ca6e0e11',
-        richtext:
-          '<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>',
-        component: 'RichtextBasic',
-        uiComponent: 'Richtext',
-        _editable:
-          '<!--#storyblok#{"name": "RichtextBasic", "space": "152017", "uid": "4d12b093-e25f-4ff2-bc8c-f645ca6e0e11", "id": "119228911"}-->',
-      },
-    ],
-    buttons: [
-      {
-        icon: 'arrow_forward',
-        size: 'normal',
-        label: 'Produkte entdecken',
-        shape: 'filled',
-        variant: 'inverted',
-        disabled: false,
-        component: 'Button',
-        prependIcon: false,
-      },
-      {
-        icon: 'arrow_forward',
-        size: 'normal',
-        label: 'Produkte entdecken',
-        shape: 'outlined',
-        variant: 'inverted',
-        disabled: false,
-        component: 'Button',
-        prependIcon: false,
-      },
-    ],
-  },
-]
+export default {
+  headline: 'Komplettlösungen für jeden Vakuumbereich',
+  component: 'HomeStageContent',
+  richtext: [
+    {
+      _uid: '4d12b093-e25f-4ff2-bc8c-f645ca6e0e11',
+      richtext:
+        '<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>',
+      component: 'RichtextBasic',
+      uiComponent: 'Richtext',
+      _editable:
+        '<!--#storyblok#{"name": "RichtextBasic", "space": "152017", "uid": "4d12b093-e25f-4ff2-bc8c-f645ca6e0e11", "id": "119228911"}-->',
+    },
+  ],
+  buttons: [
+    {
+      icon: 'arrow_forward',
+      size: 'normal',
+      label: 'Produkte entdecken',
+      shape: 'filled',
+      variant: 'inverted',
+      disabled: false,
+      component: 'Button',
+      prependIcon: false,
+    },
+    {
+      icon: 'arrow_forward',
+      size: 'normal',
+      label: 'Produkte entdecken',
+      shape: 'outlined',
+      variant: 'inverted',
+      disabled: false,
+      component: 'Button',
+      prependIcon: false,
+    },
+  ],
+}
