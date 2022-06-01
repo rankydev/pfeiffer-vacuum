d0d4ce9ddeab01d4f590f1fa2fb79add7a67ee7f
julius.schultz <julius.schultz@diva-e.com>
2022-06-01 14:04:14 +0200

    PVWEB-347 | feature | implemented lsg

--- components/organisms/Footer/partials/FooterNewsletter/FooterNewsletter.stories.js
+++ components/organisms/Footer/partials/FooterNewsletter/FooterNewsletter.stories.js
@@ -1,4 +1,27 @@
 import FooterNewsletter from './FooterNewsletter.vue'
+import {
+  newsletterHeadline,
+  newsletterRichtext,
+  newsletterButton,
+} from '~/components/organisms/Footer/partials/FooterNewsletter/FooterNewsletter.stories.content.js'
+
+const argTypes = {
+  headline: {
+    name: 'newsletterHeadline',
+    defaultValue: newsletterHeadline,
+    control: { type: 'text' },
+  },
+  richtext: {
+    name: 'newsletterRichtext',
+    defaultValue: newsletterRichtext,
+    control: { type: 'array' },
+  },
+  button: {
+    name: 'newsletterButton',
+    defaultValue: newsletterButton,
+    control: { type: 'array' },
+  },
+}
 
 export default {
   title: 'Organisms/FooterNewsletter',
@@ -10,23 +33,25 @@ export default {
           'The FooterNewsletter component is used to offer the possibility to subscribe for the newsletter in the Footer. Below the headline of the component can be defined a description which is a richtext and up to 1 button.',
       },
       source: {
-        code: '<FooterNewsletter v-bind="{}"  />',
+        code: '<FooterNewsletter v-bind="{ headline richtext button }"  />',
       },
     },
   },
+  argTypes: argTypes,
 }
 
 const Template = (args) => ({
   components: { FooterNewsletter },
   setup() {
-    return { args }
+    return {
+      args,
+    }
   },
   template: `
-  <div class="documentation-preview">
-    <FooterNewsletter v-bind="args" />
-  </div>
-`,
+    <div class="documentation-preview tw-p-8 tw-bg-pv-grey-16 tw-text-inverse">
+    <FooterNewsletter v-bind="args"/>
+    </div>
+  `,
 })
 
 export const Default = Template.bind({})
-Default.args = {}
