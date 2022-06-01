51d57065ad3e4ba829a694af8ec76095041a5f93
julius.schultz <julius.schultz@diva-e.com>
2022-06-01 14:36:54 +0200

    PVWEB-347 | feature | changed lsg content and LinkedList font sizes

--- components/organisms/Footer/partials/FooterNewsletter/FooterNewsletter.stories.content.js
+++ components/organisms/Footer/partials/FooterNewsletter/FooterNewsletter.stories.content.js
@@ -2,7 +2,7 @@ export const newsletterRichtext = [
   {
     _uid: '4d12b093-e25f-4ff2-bc8c-f645ca6e0e11',
     richtext:
-      '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam ipsum dolor sit amet</p>',
+      '<p>Sie möchten über aktuelle News von Pfeiffer Vacuum informiert werden? Dann abonnieren Sie unseren Newsletter.</p>',
     component: 'RichtextBasic',
     uiComponent: 'Richtext',
     _editable:
