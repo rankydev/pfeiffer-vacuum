8b43d3afd146ba7c23e1e27b279fcaa34b976adb
David Gonzalez Casin <david.gonzalezcasin@diva-e.com>
2022-06-03 16:33:41 +0200

    PVWEB-328 | feature | removed Quicklink component and used Link with new variant instead

--- components/molecules/OnPageNavigation/OnPageNavigation.vue
+++ components/molecules/OnPageNavigation/OnPageNavigation.vue
@@ -6,26 +6,28 @@
     <Breadcrumb v-if="!hideBreadcrumb" />
 
     <div v-if="quicklinks.length > 0" class="on-page-navigation__quicklinks">
-      <Quicklink
+      <Link
         v-for="item in quicklinks"
         :key="item._uid"
-        :anchor="item.anchor"
+        :href="`#${item.anchor}`"
+        class="tw-paragraph"
+        variant="quicklink"
       >
         {{ item.label }}
-      </Quicklink>
+      </Link>
     </div>
   </ContentWrapper>
 </template>
 
 <script>
 import { defineComponent } from '@nuxtjs/composition-api'
-import Quicklink from '~/components/molecules/Quicklink/Quicklink'
+import Link from '~/components/atoms/Link/Link'
 import ContentWrapper from '~/components/molecules/ContentWrapper/ContentWrapper.vue'
 import Breadcrumb from '~/components/molecules/Breadcrumb/Breadcrumb.vue'
 
 export default defineComponent({
   components: {
-    Quicklink,
+    Link,
     ContentWrapper,
     Breadcrumb,
   },
