1b61e042dcbc70700125355496a7689663384fb3
julius.schultz <julius.schultz@diva-e.com>
2022-06-15 14:42:13 +0200

    PVWEB-422 | feat | add initial skeleton structure

---
+++ components/atoms/FormComponents/Textarea.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  components: {},
+  props: {},
+  setup(props) {},
+})
+</script>
+
+<style lang="scss"></style>
