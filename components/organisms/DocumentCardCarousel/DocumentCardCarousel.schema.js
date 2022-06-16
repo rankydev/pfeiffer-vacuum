906abe22acf3a715a8cca99820bcfc243b71560f
Daniela Hofmann <daniela.hofmann@diva-e.com>
2022-06-16 14:30:11 +0200

    PVWEB-394 | feat | reuse ContentCarousel schema in DocumentCardCarousel.schema.js

--- components/organisms/DocumentCardCarousel/DocumentCardCarousel.schema.js
+++ components/organisms/DocumentCardCarousel/DocumentCardCarousel.schema.js
@@ -1,21 +1,10 @@
+import ContentCarousel from '~/components/organisms/ContentCarousel/ContentCarousel.schema.partial.js'
+
 export default {
   name: 'DocumentCardCarousel',
   display_name: 'Document Card Carousel',
   schema: {
-    headline: {
-      type: 'text',
-      display_name: 'Headline',
-      required: true,
-      pos: 1,
-    },
-    button: {
-      type: 'bloks',
-      display_name: 'Button',
-      restrict_components: true,
-      component_whitelist: ['Button'],
-      maximum: 1,
-      pos: 2,
-    },
+    ...ContentCarousel,
     slides: {
       type: 'bloks',
       display_name: 'Slides',
