c72ae9232d5e0d49a13e7d99b347b61d735b6232
Jessica Holst <>
2022-06-30 18:07:16 +0200

    PVWEB-417 | feature | added tests for ErrorMessage

---
+++ components/atoms/FormComponents/partials/ErrorMessage/ErrorMessage.spec.js
@@ -0,0 +1,25 @@
+import { shallowMount } from '@vue/test-utils'
+import ErrorMessage from './errorMessage.vue'
+import { expect } from '@jest/globals'
+
+describe('Input', () => {
+  describe('initial state', () => {
+    it('should render empty component when no data provided', () => {
+      const wrapper = shallowMount(ErrorMessage)
+      const errorMsg = wrapper.find('.error-message')
+      expect(errorMsg.exists()).toBeTruthy()
+    })
+
+    it('should set an error message when provided', () => {
+      const propsData = { errorMessage: 'Some error occured' }
+      const wrapper = shallowMount(ErrorMessage, { propsData })
+      const errorMsg = wrapper.find('.error-message')
+
+      expect(errorMsg.text()).toBe(propsData.errorMessage)
+    })
+  })
+
+  // describe('during interaction', () => {})
+
+  // describe('business requirements', () => {})
+})
