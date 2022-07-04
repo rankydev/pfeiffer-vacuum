fe5b211109defb7b9e592a5092ca625778449536
julius.schultz <julius.schultz@diva-e.com>
2022-07-04 13:08:40 +0200

    PVWEB-423 | feat | add tests

--- components/atoms/FormComponents/Select/PvSelect.spec.js
+++ components/atoms/FormComponents/Select/PvSelect.spec.js
@@ -1,19 +1,32 @@
 import PvSelect from '~/components/atoms/FormComponents/Select/PvSelect.vue'
-import content from './PvSelect.stories.content'
+import { normal, error, label } from './PvSelect.stories.content'
 import { shallowMount } from '@vue/test-utils'
-
-const propsSelect = {
-  ...content,
-}
+import Label from '~/components/atoms/FormComponents/partials/Label/Label'
+import errorMessage from '~/components/atoms/FormComponents/partials/ErrorMessage/ErrorMessage'
+import { expect } from '@jest/globals'
 
 describe('Select', () => {
   describe('initial state', () => {
     test('should be rendered correctly given props', () => {
-      const propsData = { ...content }
+      const propsData = { ...normal }
       const wrapper = shallowMount(PvSelect, { propsData })
       const selectWrapper = wrapper.find('.pv-select')
 
       expect(selectWrapper.exists()).toBeTruthy()
+    })
+    test('should render label component correctly given props', () => {
+      const propsData = { ...label }
+      const wrapper = shallowMount(PvSelect, { propsData })
+      const labelWrapper = wrapper.findComponent(Label)
+
+      expect(labelWrapper.attributes('label')).toBe(propsData.label)
+    })
+    it('should set an error message when provided', () => {
+      const propsData = { ...error }
+      const wrapper = shallowMount(PvSelect, { propsData })
+      const input = wrapper.findComponent(errorMessage)
+
+      expect(input.attributes('errormessage')).toBe(propsData.errorMessage)
     })
   })
 })
