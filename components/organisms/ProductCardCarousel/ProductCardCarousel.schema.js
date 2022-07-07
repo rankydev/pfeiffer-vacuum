a4963e7ac8632ac31e412f94286f89cdaa29f7f8
Daniela Hofmann <daniela.hofmann@diva-e.com>
2022-07-07 08:54:28 +0200

    PVWEB-342 | feat | enrich schema

--- components/organisms/ProductCardCarousel/ProductCardCarousel.schema.js
+++ components/organisms/ProductCardCarousel/ProductCardCarousel.schema.js
@@ -4,7 +4,16 @@ export default {
   name: 'ProductCardCarousel',
   display_name: 'ProductCardCarousel',
   schema: {
-    ...ContentCarousel.schema,
+    ...ContentCarousel,
+    slides: {
+      type: 'bloks',
+      display_name: 'Slides',
+      restrict_components: true,
+      component_whitelist: ['ProductCard'],
+      maximum: 16,
+      required: true,
+      pos: 3,
+    },
   },
   image: null,
   preview_field: null,
