8e7486796803e3e395ab57caba1cb04e0ad728b3
Jessica Holst <>
2022-07-08 14:34:31 +0200

    PVWEB-689 | feat | added test file

---
+++ components/organisms/ContactRequestSelection/ContactRequestSelection.spec.js
@@ -0,0 +1,157 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import ContactRequestSelection from './ContactRequestSelection.vue'
+import {
+  mainHeadline,
+  selectionHeadline,
+  text,
+  contactRequests,
+} from './ContactRequestSelection.stories.content.js'
+
+let wrapper
+
+function createComponent(propsData = {}) {
+  const stubs = { NuxtDynamic: true }
+  // const localVue = createLocalVue()
+  // const editable = (el, key) => (el.innerText = key.value)
+  // localVue.directive('editable', editable)
+
+  const options = {
+    // localVue,
+    stubs,
+    propsData,
+  }
+
+  wrapper = shallowMount(ContactRequestSelection, options)
+}
+
+describe('ContactRequestSelection', () => {
+  describe('initial state', () => {
+    it('should provide headline subline and richtext to richtext component when these are provided', () => {
+      const propsData = { headline, subline, richtext }
+      createComponent(propsData)
+      const domRichtext = wrapper.findComponent(Richtext)
+
+      expect(domRichtext.vm.richtext).toMatch(headline)
+      expect(domRichtext.vm.richtext).toMatch(subline)
+      expect(domRichtext.vm.richtext).toMatch(richtext)
+    })
+
+    it('should add buttons to the button area when they are provided', () => {
+      const propsData = { buttons }
+      createComponent(propsData)
+
+      const domButtons = wrapper.findAll('.content-media-block__button')
+
+      expect(domButtons.length).toBe(buttons.length)
+      domButtons.wrappers.forEach((domButton, idx) => {
+        expect(domButton.attributes('component')).toBe(buttons[idx].component)
+      })
+    })
+
+    it('should add Video to the media area when it is provided', () => {
+      const propsData = { richtext, media: mediaVideo }
+      createComponent(propsData)
+
+      const domMedia = wrapper.find('.content-media-block__media')
+
+      expect(domMedia.html()).toContain(mediaVideo[0].component)
+    })
+
+    it('should add Image to the media area when it is provided', () => {
+      const propsData = { richtext, media: mediaImage }
+      createComponent(propsData)
+
+      const domMedia = wrapper.find('.content-media-block__media')
+
+      expect(domMedia.html()).toContain(mediaImage[0].component)
+    })
+
+    it('should add CTABox to the media area when it is provided', () => {
+      const propsData = { richtext, media: mediaCTA }
+      createComponent(propsData)
+
+      const domMedia = wrapper.find('.content-media-block__media')
+
+      expect(domMedia.html()).toContain(mediaCTA[0].component)
+    })
+
+    it('should apply correct classes given a half/half ratio', () => {
+      const propsData = { richtext, media: mediaVideo, ratio: 'half/half' }
+      createComponent(propsData)
+
+      const domContent = wrapper.find('.content-media-block__content')
+      const domMedia = wrapper.find('.content-media-block__media')
+
+      expect(domContent.attributes('class')).toMatch(
+        'content-media-block__content--half'
+      )
+      expect(domMedia.attributes('class')).toMatch(
+        'content-media-block__media--half'
+      )
+    })
+
+    it('should apply correct classes given a one/three ratio', () => {
+      const propsData = { richtext, media: mediaVideo, ratio: 'one/three' }
+      createComponent(propsData)
+
+      const domContent = wrapper.find('.content-media-block__content')
+      const domMedia = wrapper.find('.content-media-block__media')
+
+      expect(domContent.attributes('class')).toMatch(
+        'content-media-block__content--one'
+      )
+      expect(domMedia.attributes('class')).toMatch(
+        'content-media-block__media--three'
+      )
+    })
+
+    it('should apply correct classes given a three/one ratio', () => {
+      const propsData = { richtext, media: mediaVideo, ratio: 'three/one' }
+      createComponent(propsData)
+
+      const domContent = wrapper.find('.content-media-block__content')
+      const domMedia = wrapper.find('.content-media-block__media')
+
+      expect(domContent.attributes('class')).toMatch(
+        'content-media-block__content--three'
+      )
+      expect(domMedia.attributes('class')).toMatch(
+        'content-media-block__media--one'
+      )
+    })
+
+    it('should apply correct classes given a content/media order', () => {
+      const propsData = { richtext, media: mediaVideo, order: 'content/media' }
+      createComponent(propsData)
+
+      const domContent = wrapper.find('.content-media-block__content')
+      const domMedia = wrapper.find('.content-media-block__media')
+
+      expect(domContent.attributes('class')).toMatch(
+        'content-media-block__content--first'
+      )
+      expect(domMedia.attributes('class')).toMatch(
+        'content-media-block__media--last'
+      )
+    })
+
+    it('should apply correct classes given a media/content order', () => {
+      const propsData = { richtext, media: mediaVideo, order: 'media/content' }
+      createComponent(propsData)
+
+      const domContent = wrapper.find('.content-media-block__content')
+      const domMedia = wrapper.find('.content-media-block__media')
+
+      expect(domContent.attributes('class')).toMatch(
+        'content-media-block__content--last'
+      )
+      expect(domMedia.attributes('class')).toMatch(
+        'content-media-block__media--first'
+      )
+    })
+  })
+
+  // describe('during interaction', () => {})
+
+  // describe('business requirements', () => {})
+})
--- components/organisms/ContactRequestSelection/ContactRequestSelection.stories.content.js
+++ components/organisms/ContactRequestSelection/ContactRequestSelection.stories.content.js
@@ -2,7 +2,8 @@ import { accordionEntries } from '~/components/atoms/Accordion/Accordion.stories
 
 export const mainHeadline = 'Kontakt'
 export const selectionHeadline = 'Ihre Anfrage'
-export const text = 'Salami'
+export const text =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Non, libero sapien sodales placerat lorem tincidunt.'
 export const contactRequests = accordionEntries
 
 export default {}
