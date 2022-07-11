eaf96e0465ef9dc6c372741e9299974ac23bfed4
philipp.timmel <philipp.timmel@diva-e.com>
2022-07-11 17:08:57 +0200

    PVWEB-692 | feat | Add test for type emit

--- components/molecules/ContactRequestSubject/ContactRequestSubject.spec.js
+++ components/molecules/ContactRequestSubject/ContactRequestSubject.spec.js
@@ -28,4 +28,23 @@ describe('ContactRequestSubject', () => {
       expect(description.exists()).toBeTruthy()
     })
   })
+
+  describe('during interaction', () => {
+    test('should emit type when clicking the subject', async () => {
+      const propsData = content
+      const wrapper = shallowMount(ContactRequestSubject, {
+        stubs: { NuxtDynamic: true },
+        propsData,
+      })
+
+      const emitMock = jest.fn()
+      const subject = wrapper.find('.contact-request-subject')
+      wrapper.vm.$emit = emitMock
+
+      await subject.trigger('click')
+
+      expect(emitMock).toBeCalledTimes(1)
+      expect(emitMock).toBeCalledWith('selected', content.type)
+    })
+  })
 })
