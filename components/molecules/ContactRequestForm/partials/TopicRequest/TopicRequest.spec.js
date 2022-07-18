72e0cb963ef2eb7875f88c6b6c6c2f42eea19635
julius.schultz <julius.schultz@diva-e.com>
2022-07-18 13:01:19 +0200

    PVWEB-667 | feat | adjust names

--- components/molecules/ContactRequestForm/partials/TopicRequest/TopicRequest.spec.js
+++ components/molecules/ContactRequestForm/partials/TopicRequest/TopicRequest.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import TopicRequest from '~/components/molecules/ContactRequestForm/partials/TopicRequest/TopicRequest'
 import PvInput from '~/components/atoms/FormComponents/PvInput/PvInput'
 import PvSelect from '~/components/atoms/FormComponents/PvSelect/PvSelect'
-import PvTextarea from '~/components/atoms/FormComponents/PvTextarea/PvTextarea'
+import PvTextArea from '~/components/atoms/FormComponents/PvTextArea/PvTextArea'
 
 describe('GeneralRequest', () => {
   describe('initial state', () => {
@@ -10,7 +10,7 @@ describe('GeneralRequest', () => {
       const wrapper = shallowMount(TopicRequest)
       const inputArr = wrapper.findAllComponents(PvInput)
       const select = wrapper.findComponent(PvSelect)
-      const textarea = wrapper.findComponent(PvTextarea)
+      const textarea = wrapper.findComponent(PvTextArea)
 
       expect(wrapper.exists()).toBeTruthy()
       expect(inputArr).toHaveLength(8)
