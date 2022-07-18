fb49795f025117af31795ed35bcdc8c12486711b
Jessica Holst <>
2022-07-18 16:53:02 +0200

    PVWEB-697 | feat | added test case for coverage if-else

--- components/molecules/ContactRequestForm/ContactRequestForm.spec.js
+++ components/molecules/ContactRequestForm/ContactRequestForm.spec.js
@@ -1,14 +1,26 @@
 import { shallowMount } from '@vue/test-utils'
 import ContactRequestForm from '~/components/molecules/ContactRequestForm/ContactRequestForm'
+import GeneralRequest from '~/components/molecules/ContactRequestForm/partials/GeneralRequest/GeneralRequest'
+import TopicRequest from '~/components/molecules/ContactRequestForm/partials/TopicRequest/TopicRequest'
 
-const propsData = { contactRequestType: 'QUOTE' }
+// const propsData = { contactRequestType: 'QUOTE' }
 
 describe('ContactRequestForm', () => {
   describe('initial state', () => {
     test('should render correctly', () => {
+      const propsData = { contactRequestType: 'GENERAL_QUERY' }
       const wrapper = shallowMount(ContactRequestForm, { propsData })
+      const generalForm = wrapper.findComponent(GeneralRequest)
 
+      expect(generalForm.exists()).toBeTruthy()
       expect(wrapper.exists()).toBeTruthy()
+    })
+    test('should render correctly', () => {
+      const propsData = { contactRequestType: 'SERVICE_REQUEST' }
+      const wrapper = shallowMount(ContactRequestForm, { propsData })
+      const topicForm = wrapper.findComponent(TopicRequest)
+
+      expect(topicForm.exists()).toBeTruthy()
     })
   })
 })
