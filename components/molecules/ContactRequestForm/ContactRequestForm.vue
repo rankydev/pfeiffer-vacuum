43425d2dc76f11f985885d1acf692912997a4914
julius.schultz <julius.schultz@diva-e.com>
2022-07-18 13:22:44 +0200

    PVWEB-667 | feat | add ContactRequestForm

---
+++ components/molecules/ContactRequestForm/ContactRequestForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <GeneralRequest />
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import GeneralRequest from '~/components/molecules/ContactRequestForm/partials/GeneralRequest/GeneralRequest'
+
+export default defineComponent({
+  components: {
+    GeneralRequest,
+  },
+  props: {
+    /**
+     * The image
+     */
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss"></style>
