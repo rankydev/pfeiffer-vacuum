6d19d898afadf2f60ecb4b7d93db95d0d1f15639
Jessica Holst <>
2022-08-01 16:19:54 +0200

    PVWEB-434 | feat | created base for adding whole component to lsg later

---
+++ components/molecules/GenericModal/GenericModal.stories.js
@@ -0,0 +1,31 @@
+import GenericModal from './GenericModal'
+import modalContent from './GenericModal.stories.content'
+
+const argTypes = {}
+
+export default {
+  title: 'Technical components/Generic Modal',
+  component: GenericModal,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Generic modal component used for various modal modules',
+      },
+    },
+  },
+  argTypes,
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { GenericModal },
+  props: Object.keys(argTypes),
+  setup() {
+    return { args }
+  },
+  template: `<div>
+    <GenericModal v-bind="args" />
+  </div>`,
+})
+
+export const Default = Template.bind({})
+Default.args = {}
---
+++ components/molecules/InformationModal/InformationModal.stories.js
@@ -0,0 +1,32 @@
+import InformationModal from './InformationModal'
+
+const argTypes = {}
+
+export default {
+  title: 'Technical components/Information Modal',
+  component: InformationModal,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Generic modal component used for various modal modules',
+      },
+    },
+  },
+  argTypes,
+}
+
+const Template = (args, { argTypes }) => ({
+  components: { InformationModal },
+  props: Object.keys(argTypes),
+  setup() {
+    return { args }
+  },
+  template: `<div>
+  <GenericModal>
+    <InformationModal v-bind="args" />
+    </GenericModal>
+  </div>`,
+})
+
+export const Default = Template.bind({})
+Default.args = {}
