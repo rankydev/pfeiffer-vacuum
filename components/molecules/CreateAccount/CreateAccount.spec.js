fe4038adb5511768d7ce07c507af7fc90dc832fe
julius.schultz <julius.schultz@diva-e.com>
2022-08-23 16:54:21 +0200

    PVWEB-744 | fix | fix tests

--- components/molecules/CreateAccount/CreateAccount.spec.js
+++ components/molecules/CreateAccount/CreateAccount.spec.js
@@ -1,23 +1,10 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import CreateAccount from '~/components/molecules/CreateAccount/CreateAccount'
 import PvInput from '~/components/atoms/FormComponents/PvInput/PvInput'
 import PvSelect from '~/components/atoms/FormComponents/PvSelect/PvSelect'
 import Password from '~/components/atoms/FormComponents/Password/Password'
 import { setActivePinia, createPinia } from 'pinia'
 import { ref } from '@nuxtjs/composition-api'
-
-const localVue = createLocalVue()
-localVue.prototype.$nuxt = {
-  context: {
-    $hybrisApi: {
-      countriesApi: {
-        getCountries: jest.fn(() => {
-          return { then: (i) => i }
-        }),
-      },
-    },
-  },
-}
 
 const mockLoadCountries = jest.fn()
 jest.mock('~/stores/misc', () => {
@@ -51,7 +38,7 @@ describe('CreateAccount', () => {
   describe('initial state', () => {
     beforeEach(() => setActivePinia(createPinia()))
     test('should render component correctly', () => {
-      const wrapper = shallowMount(CreateAccount, { localVue })
+      const wrapper = shallowMount(CreateAccount)
       const title = wrapper.find('h2')
       const inputArr = wrapper.findAllComponents(PvInput)
       const select = wrapper.findComponent(PvSelect)
@@ -66,59 +53,26 @@ describe('CreateAccount', () => {
 
     test('should validate input fields given validate propsData', () => {
       const propsData = { validate: true }
-      const wrapper = shallowMount(CreateAccount, { propsData, localVue })
+      const wrapper = shallowMount(CreateAccount, { propsData })
 
       expect(wrapper.exists()).toBeTruthy()
     })
   })
   describe('during interaction', () => {
-    it('should return regions array when isocode is provided', async () => {
-      const regions = ['Region1']
-
-      const wrapper = shallowMount(CreateAccount, {
-        localVue,
-      })
-
-      wrapper.setData({
-        requestData: {
-          registration: {
-            company: '',
-            address: {
-              country: {
-                isocode: 'US',
-                name: 'United States of America',
-              },
-              region: {},
-            },
-            firstName: '',
-            lastName: '',
-            email: '',
-            password: '',
-          },
-        },
-      })
-
-      await wrapper.vm.loadRegions()
-      expect(wrapper.vm.regions).toStrictEqual(regions)
-    })
-
     it('should return regions array when appropriate country was selected', async () => {
-      const wrapper = shallowMount(CreateAccount, {
-        localVue,
-      })
+      const wrapper = shallowMount(CreateAccount)
       const select = wrapper.findComponent(PvSelect)
 
-      const selectedOption = { isocode: 'US', name: 'United States of America' }
-      await select.setData({ internalValue: selectedOption })
+      const selectedOption = {
+        isocode: 'US',
+        name: 'United States of America',
+      }
+      select.vm.$emit('update', selectedOption)
       await select.vm.$nextTick()
-      await wrapper.vm.$nextTick()
-
       const allSelects = wrapper.findAllComponents(PvSelect)
 
-      console.log(wrapper.vm.regions)
-
-      //expect(mockLoadRegions).toHaveBeenCalled()
-      // expect(allSelects).toHaveLength(2)
+      expect(mockLoadRegions).toHaveBeenCalled()
+      expect(allSelects).toHaveLength(2)
     })
   })
 })
