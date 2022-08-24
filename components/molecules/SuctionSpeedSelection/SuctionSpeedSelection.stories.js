4a4d799d5a154676a68eb76ee7b8002f5ff4821e
julius.schultz <julius.schultz@diva-e.com>
2022-08-24 16:41:40 +0200

    PVWEB-456 | feat | add calculated values to story

--- components/molecules/SuctionSpeedSelection/SuctionSpeedSelection.stories.js
+++ components/molecules/SuctionSpeedSelection/SuctionSpeedSelection.stories.js
@@ -1,4 +1,5 @@
 import SuctionSpeedSelection from '~/components/molecules/SuctionSpeedSelection/SuctionSpeedSelection.vue'
+import { ref } from '@nuxtjs/composition-api'
 
 export default {
   title: 'Molecules/m_127 SuctionSpeedSelection',
@@ -14,9 +15,23 @@ export default {
 
 const Template = () => ({
   components: { SuctionSpeedSelection },
+  setup() {
+    let result = ref([])
+
+    const initializeResult = (e) => {
+      result.value = e
+      console.log(result.value)
+    }
+
+    return { result, initializeResult }
+  },
   template: `
   <div class="documentation-preview">
-    <SuctionSpeedSelection />
+    <SuctionSpeedSelection
+      @input="initializeResult($event)" />
+    <br />
+    <div>Minimum: {{ result[0] }}</div>
+    <div>Maximum: {{ result[1] }}</div>
   </div>
 `,
 })
