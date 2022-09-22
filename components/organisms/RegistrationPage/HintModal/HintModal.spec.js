af3c27929d5041870bd984565a47f29910e6d790
Daniela Hofmann <daniela.hofmann@diva-e.com>
2022-09-22 19:29:04 +0200

    PVWEB-746 | feat | add more tests for HintModal.vue

--- components/organisms/RegistrationPage/HintModal/HintModal.spec.js
+++ components/organisms/RegistrationPage/HintModal/HintModal.spec.js
@@ -1,6 +1,8 @@
 import HintModal from './HintModal'
 import { shallowMount } from '@vue/test-utils'
 import { expect } from '@jest/globals'
+import Button from '~/components/atoms/Button/Button'
+import Link from '~/components/atoms/Link/Link'
 
 describe('HintModal', () => {
   describe('initial state', () => {
@@ -22,6 +24,39 @@ describe('HintModal', () => {
       const component = wrapper.find('.modal')
 
       expect(component.exists()).toBeFalsy()
+    })
+  })
+
+  describe('during interaction', () => {
+    test('should emit closeModal event with true when proceed-button is clicked', async () => {
+      const wrapper = shallowMount(HintModal, {
+        propsData: { isOpen: true },
+      })
+
+      const button = wrapper.findComponent(Button)
+      button.vm.$emit('click')
+      await wrapper.vm.$nextTick()
+
+      expect(wrapper.emitted().closeModal.length).toBe(1)
+      expect(wrapper.emitted().closeModal[0][0]).toEqual({
+        proceedWithoutCompany: true,
+      })
+    })
+
+    test('should emit closeModal event with val false when link is clicked', async () => {
+      const wrapper = shallowMount(HintModal, {
+        propsData: { isOpen: true },
+      })
+
+      const link = wrapper.findComponent(Link)
+      const button = link.findComponent(Button)
+      button.vm.$emit('click')
+      await wrapper.vm.$nextTick()
+
+      expect(wrapper.emitted().closeModal.length).toBe(1)
+      expect(wrapper.emitted().closeModal[0][0]).toEqual({
+        proceedWithoutCompany: false,
+      })
     })
   })
 })
