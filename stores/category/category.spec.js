ab9f43e7a095f7bf0be176d577cd245e1f7737ff
julius.schultz <julius.schultz@diva-e.com>
2022-09-26 11:49:54 +0200

    PVWEB-450 | feat | work on tests for category.spec.js

--- stores/category/category.spec.js
+++ stores/category/category.spec.js
@@ -1,20 +1,30 @@
 import { useCategoryStore } from '~/stores/category'
 import { setActivePinia, createPinia } from 'pinia'
+import { entries } from '~/components/molecules/Breadcrumb/Breadcrumb.stories.content'
 
 jest.mock('@nuxtjs/composition-api', () => {
   const originalModule = jest.requireActual('@nuxtjs/composition-api')
   const { ref } = originalModule
 
   return {
+    ...originalModule,
     useContext: jest.fn(() => {
       return {
         $axios: {
           get: jest.fn(() => {
-            return { then: jest.fn() }
+            return {
+              then: jest.fn(() => {
+                return 'Test'
+              }),
+            }
           }),
         },
-        i18n: {},
-        app: {},
+        i18n: { locale: 'en' },
+        app: {
+          localePath: jest.fn((input) => {
+            return ''
+          }),
+        },
       }
     }),
     useRouter: jest.fn(() => {
@@ -25,22 +35,11 @@ jest.mock('@nuxtjs/composition-api', () => {
         value: {
           fullPath: '',
           params: { category: '' },
+          query: {},
         },
       }
     }),
     ssrRef: ref,
-    useAsync: jest.fn(() => {
-      return {}
-    }),
-    unref: jest.fn(() => {
-      return {}
-    }),
-    computed: jest.fn(() => {
-      return {}
-    }),
-    ref: jest.fn(() => {
-      return {}
-    }),
   }
 })
 
@@ -61,17 +60,30 @@ describe('useCategoryStore', () => {
     test('should return all expected properties', async () => {
       const categoryStore = await useCategoryStore()
 
-      expect(categoryStore.category).toStrictEqual({})
-      expect(categoryStore.result).toStrictEqual({})
+      expect(categoryStore.category).toBe(null)
+      expect(categoryStore.result).toBe(null)
+      expect(categoryStore.breadcrumb).toStrictEqual([entries[0], entries[1]])
+      expect(categoryStore.categoryName).toBe(entries[1].name)
+      expect(categoryStore.parentCategoryPath).toBe(null)
       expect(categoryStore.loadByPath).toBeInstanceOf(Function)
     })
   })
 
   describe('during interaction', () => {
-    test('should execute loadByPath method correctly', async () => {
+    test('should execute loadByPath correctly', async () => {
       const categoryStore = await useCategoryStore()
 
       await categoryStore.loadByPath()
+      expect(categoryStore.category).toBe('Test')
+      expect(categoryStore.result).toBe('Test')
+    })
+
+    test('should return ... given categoryPath', async () => {
+      const categoryStore = await useCategoryStore()
+
+      await categoryStore.loadByPath()
+
+      expect(categoryStore.parentCategoryPath).toBe(null)
     })
   })
 })
