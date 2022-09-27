c0dc2538c86d4ad76bfb2f89dded4314500c0909
julius.schultz <julius.schultz@diva-e.com>
2022-09-27 15:00:09 +0200

    PVWEB-450 | feat | resolve Merge conflicts

--- plugins/hybris/hybris.config.js
+++
@@ -1,7 +0,0 @@
-const CATEGORIES_API = BASE_API_PATH + '/catalogs/pfeifferProductCatalog/Online'
-
-export default {
-  // Categories
-  CATEGORIES_API,
-  CATEGORY_API: CATEGORIES_API + '/categories',
-}
