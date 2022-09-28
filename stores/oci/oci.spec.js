45699702ca21c790843bc8484ff5dc8270c4890f
julius.schultz <julius.schultz@diva-e.com>
2022-09-28 17:44:41 +0200

    PVWEB-741 | fix | add oci tests

---
+++ stores/oci/oci.spec.js
@@ -0,0 +1,35 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { useOciStore } from './oci'
+
+const mockAuth = {
+  type: 'oci',
+}
+
+describe('Oci store', () => {
+  beforeEach(() => setActivePinia(createPinia()))
+
+  describe('initial state', () => {
+    test('should return all expected properties', () => {
+      const ociStore = useOciStore()
+
+      expect(ociStore.isOciPage).toBe(false)
+      expect(ociStore.hookUrl).toBe('')
+      expect(ociStore.returnTarget).toBe('_self')
+      expect(ociStore.hiddenUIElements).toStrictEqual({})
+      expect(ociStore.customerId).toBe('')
+      expect(ociStore.isOciUser).toBeInstanceOf(Function)
+    })
+  })
+
+  describe('during interaction', () => {
+    test('should', async () => {
+      const ociStore = useOciStore()
+
+      const result = ociStore.isOciUser(mockAuth)
+
+      expect(result).toBe(true)
+    })
+  })
+
+  // describe('business requirements', () => {})
+})
