a54c4ce660cc0dd6f5bd56be111314e2619dc445
Jessica Holst <>
2022-09-29 17:50:26 +0200

    PVWEB-558 | feat | showing products in SearchResult LSG

--- components/molecules/SearchResult/SearchResult.stories.js
+++ components/molecules/SearchResult/SearchResult.stories.js
@@ -1,5 +1,26 @@
 import SearchResult from '~/components/molecules/SearchResult/SearchResult.vue'
 import { products, pagination } from './SearchResult.stories.content.js'
+import {
+  productWithOrderNumber,
+  productWithVariants,
+} from '../../molecules/ProductCard/ProductCard.stories.content'
+
+const argTypes = {
+  products: {
+    control: { type: 'array' },
+    defaultValue: [
+      productWithOrderNumber,
+      productWithVariants,
+      productWithOrderNumber,
+      productWithVariants,
+      productWithOrderNumber,
+      productWithVariants,
+      productWithOrderNumber,
+      productWithVariants,
+      productWithOrderNumber,
+    ],
+  },
+}
 
 export default {
   title: 'Molecules/m_xxx SearchResult',
@@ -11,19 +32,23 @@ export default {
       },
     },
   },
+  argTypes: argTypes,
 }
 
-const Template = (args) => ({
+const Template = (args, { argTypes }) => ({
   components: { SearchResult },
-  setup() {},
+  setup() {
+    return { args }
+  },
   template: `
   <div class="documentation-preview">
-    <SearchResult v-bind="{ products, pagination }" />
+    <SearchResult v-bind="args" />
   </div>
 `,
 })
 
-export const Default = Template.bind({
+export const Default = Template.bind({})
+Default.args = {
   products,
   pagination,
-})
+}
