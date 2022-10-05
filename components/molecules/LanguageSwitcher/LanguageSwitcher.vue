a60c21fdc6c2b99ec964308598e6c9635d21c331
julius.schultz <julius.schultz@diva-e.com>
2022-10-05 17:56:20 +0200

    PVWEB-741 | feat | add tailwind classes

--- components/molecules/LanguageSwitcher/LanguageSwitcher.vue
+++ components/molecules/LanguageSwitcher/LanguageSwitcher.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="language-switcher">
-    <div class="wrapper">
-      <div class="content">
+    <div class="language-switcher__wrapper">
+      <div class="language-switcher__content">
         <ul>
           <a href="#"><li>Deutsch</li></a>
           <a href="#"><li>English</li></a>
@@ -43,57 +43,55 @@ export default defineComponent({
     padding: 0;
   }
 
-  .wrapper {
-    position: relative;
+  &__wrapper {
+    @apply tw-relative;
   }
 
-  .parent {
-    width: 100%;
+  &__parent {
+    @apply tw-w-full;
   }
 
-  .content {
-    position: absolute;
+  &__content {
+    @apply tw-absolute;
     top: 0;
     height: 0;
-    box-shadow: none;
     width: 204px;
     padding-top: 30px;
     transition: all 0.5s ease;
-    transition-delay: 0.2s;
+    @apply tw-delay-200;
   }
 
-  .wrapper:active .content,
-  .content:hover {
+  &__wrapper:active .content,
+  &__content:hover {
     height: 270px;
     transition-delay: 0s;
   }
 
-  .content:hover ul,
-  .wrapper:active .content ul {
+  &__content:hover ul,
+  &__wrapper:active .content ul {
     @apply tw-border-2;
     @apply tw-border-pv-grey-80;
     @apply tw-shadow-black-15;
     transition-delay: 0s;
   }
 
-  .content ul {
-    overflow: hidden;
-    height: 100%;
+  &__content ul {
+    @apply tw-overflow-hidden;
+    @apply tw-h-full;
     @apply tw-p-0;
     @apply tw-mt-1;
     @apply tw-rounded-md;
     @apply tw-static;
-    transition-delay: 0.2s;
-    background: #fff;
+    @apply tw-delay-200;
+    @apply tw-bg-pv-white;
   }
 
-  .content li:hover {
+  &__content li:hover {
     @apply tw-bg-pv-red-lighter;
     @apply tw-text-pv-white;
   }
 
-  .content li {
-    color: #888;
+  &__content li {
     font-size: 14px;
     padding-left: 10px;
     @apply tw-text-base;
