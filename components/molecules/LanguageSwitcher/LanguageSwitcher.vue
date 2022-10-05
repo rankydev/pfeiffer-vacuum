70c45768363c2803a6d4a61bcd0581a2b06896a8
julius.schultz <julius.schultz@diva-e.com>
2022-10-05 17:11:30 +0200

    PVWEB-741 | feat | adapted design of language switch

--- components/molecules/LanguageSwitcher/LanguageSwitcher.vue
+++ components/molecules/LanguageSwitcher/LanguageSwitcher.vue
@@ -1,13 +1,26 @@
 <template>
-  <Button
-    :label="$t('language.code')"
-    variant="inverted"
-    shape="outlined"
-    gap="narrow"
-    icon="language"
-    :prepend-icon="true"
-    class="language-switcher__button"
-  />
+  <div id="demo">
+    <div class="wrapper">
+      <div class="content">
+        <ul>
+          <a href="#"><li>Deutsch</li></a>
+          <a href="#"><li>English</li></a>
+          <a href="#"><li>Español</li></a>
+          <a href="#"><li>中国人</li></a>
+          <a href="#"><li>한국인</li></a>
+        </ul>
+      </div>
+      <Button
+        :label="$t('language.code')"
+        variant="inverted"
+        shape="outlined"
+        gap="narrow"
+        icon="language"
+        :prepend-icon="true"
+        class="language-switcher__button"
+      />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,5 +42,65 @@ export default defineComponent({
     color: #7a7a7a !important;
     padding: 0;
   }
+}
+
+#demo .wrapper {
+  position: relative;
+}
+
+#demo .parent {
+  width: 100%;
+}
+
+#demo .content {
+  position: absolute;
+  top: 0;
+  height: 0;
+  box-shadow: none;
+  width: 204px;
+  padding-top: 30px;
+  transition: all 0.5s ease;
+  transition-delay: 0.2s;
+}
+
+#demo .wrapper:active .content,
+#demo .content:hover {
+  height: 270px;
+  transition-delay: 0s;
+}
+
+#demo .content:hover ul,
+#demo .wrapper:active .content ul {
+  @apply tw-border-2;
+  @apply tw-border-pv-grey-80;
+  @apply tw-shadow-black-15;
+  transition-delay: 0s;
+}
+
+#demo .content ul {
+  overflow: hidden;
+  height: 100%;
+  @apply tw-p-0;
+  @apply tw-mt-1;
+  @apply tw-rounded-md;
+  @apply tw-static;
+  transition-delay: 0.2s;
+  background: #fff;
+}
+
+#demo .content li:hover {
+  @apply tw-bg-pv-red-lighter;
+  @apply tw-text-pv-white;
+}
+
+#demo .content li {
+  color: #888;
+  font-size: 14px;
+  padding-left: 10px;
+  @apply tw-text-base;
+  @apply tw-leading-6;
+  @apply tw-py-3;
+  @apply tw-px-4;
+  @apply tw-text-pv-black;
 }
 </style>
