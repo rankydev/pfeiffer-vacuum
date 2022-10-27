36821c9d90ef1c1eb001a3edcd6b31e927ee33b2
Robert Heinrichsen <robert.heinrichsen@diva-e.com>
2022-10-27 10:30:06 +0200

    PVWEB-779 | feat | Fix bubble migration script

--- migrations/change_Bubble_richtext.js
+++ migrations/change_Bubble_richtext.js
@@ -1,9 +1,8 @@
 module.exports = function (block) {
   if (block.richtext.length > 0) {
-    let newRichtext = {
+    block.richtext = {
       type: 'doc',
       content: block.richtext[0].richtext.content,
     }
-    block.richtext = newRichtext
   }
 }
