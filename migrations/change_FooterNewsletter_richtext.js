c50e3b30ff28ee69ae9dbd60f0bd8fa07ef5fede
Robert Heinrichsen <robert.heinrichsen@diva-e.com>
2022-10-27 11:32:45 +0200

    PVWEB-779 | feat | Remove migration scripts from code. We put these into confluence for release documentation.

--- migrations/change_FooterNewsletter_richtext.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (block) {
-  if (block.richtext.length > 0) {
-    block.richtext = {
-      type: 'doc',
-      content: block.richtext[0].richtext.content,
-    }
-  }
-}
--- migrations/change_ImageWithDescription_description.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (block) {
-  if (block.description.length > 0) {
-    block.description = {
-      type: 'doc',
-      content: block.description[0].richtext.content,
-    }
-  }
-}
