264d8d3dbdf7763cb7faffcdcf2e2a3e18f9d839
Jessica Holst <>
2022-11-03 09:05:07 +0100

    PVWEB-801 | fix | remove StickyBar from Page schema

--- components/templates/Page/Page.schema.js
+++ components/templates/Page/Page.schema.js
@@ -62,12 +62,6 @@ export default {
       restrict_components: true,
       component_whitelist: ['HelpContainer'],
     },
-    stickyBar: {
-      type: 'bloks',
-      display_name: 'Sticky Bar',
-      restrict_components: true,
-      component_whitelist: ['StickyBar'],
-    },
     ...Seo,
     ...Social,
     ...Template,
