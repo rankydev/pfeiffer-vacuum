9c602f11e86108b0602ff0af78fe0a42c6b96645
Jessica Holst <>
2022-11-08 13:05:35 +0100

    PVWEB-818 | fix | outcommented new lintin rules, to check if pipeline runs

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,7 @@ module.exports = {
     'unused-imports/no-unused-imports': 'warn',
     'unused-imports/no-unused-vars': 'warn',
     'no-restricted-imports': ['error', '@jest/globals'],
-    'no-empty-function': 'warn',
-    'no-shadow': 'warn',
+    // 'no-empty-function': 'warn',
+    // 'no-shadow': 'warn',
   },
 }
