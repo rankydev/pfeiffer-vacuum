41617f43401c9e660edeb378d12a0a8906293f12
Jessica Holst <>
2022-11-15 15:50:04 +0100

    PVWEB-465 | feat | removed placeholder div in _product Page

--- pages/shop/products/_product.vue
+++ pages/shop/products/_product.vue
@@ -47,12 +47,6 @@
             >
               Recommended Accessories
             </div>
-
-            <div
-              class="tw-bg-pv-grey-88 tw-w-full tw-rounded-lg"
-              :class="'tw-flex tw-items-center tw-justify-center tw-font-bold tw-text-pv-white tw-text-5xl tw-text-center'"
-              style="height: 700px"
-            ></div>
             <DetailTabs
               :product="productStore.product"
               :product-code="productStore.code"
