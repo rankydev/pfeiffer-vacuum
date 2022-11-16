c4783d01c540195886c863705623b808f6eb29b8
julius.schultz <julius.schultz@diva-e.com>
2022-11-16 11:28:55 +0100

    PVWEB-479 | feat | basic implementation

---
+++ components/molecules/ProductTechnicalData/ProductTechnicalData.stories.content.js
@@ -0,0 +1,7 @@
+const feature1 = {
+  name: 'Featurename',
+  featuresValues: [],
+  featureUnit: { name: 'Einheit' },
+}
+
+export const features = [feature1, feature1, feature1]
---
+++ components/molecules/ProductTechnicalData/ProductTechnicalData.vue
@@ -0,0 +1,86 @@
+<template>
+  <ul v-if="(features || []).length > 0" class="technical-data">
+    <div>Halo</div>
+    <li
+      v-for="(feature, index) in features"
+      :key="index"
+      class="technical-data__feature"
+    >
+      <div class="technical-data__child">
+        <span v-html="sanitizer.inline(feature.name || '')" />
+      </div>
+      <div class="technical-data__child">
+        <span
+          v-for="(el, i) in feature.featureValues"
+          :key="genKey(i)"
+          v-html="sanitizer.block(getFeatureText(feature, el))"
+        />
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { useSanitizer } from '~/composables/sanitizer/useSanitizer'
+
+export default {
+  name: 'ProductTechnicalData',
+  props: {
+    features: {
+      type: Array,
+      required: true,
+    },
+  },
+  setup() {
+    const sanitizer = useSanitizer()
+
+    const getFeatureText = (feature, el) => {
+      return el.value ? `${el.value} ${feature.featureUnit?.name || ''}` : ''
+    }
+
+    return { sanitizer, getFeatureText }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.technical-data {
+  @apply tw-grid tw-grid-cols-12;
+  @apply tw-gap-x-8;
+  @apply tw-py-8 tw-px-4;
+  @apply tw-list-none;
+
+  &__feature {
+    @apply tw-grid tw-grid-cols-12;
+    @apply tw-col-span-12 lg:tw-col-span-6;
+    @apply tw-bg-pv-grey-96;
+    @apply tw-p-4;
+
+    &:nth-child(even) {
+      @apply tw-bg-pv-white;
+    }
+
+    @screen lg {
+      // Override color with help of specific selector
+      &:nth-child(n) {
+        @apply tw-bg-pv-grey-96;
+      }
+
+      &:nth-child(4n-1),
+      &:nth-child(4n) {
+        @apply tw-bg-pv-white;
+      }
+    }
+  }
+
+  &__child {
+    @apply tw-col-span-6;
+    @apply tw-text-left;
+    @apply tw-break-words;
+
+    span {
+      @apply tw-block;
+    }
+  }
+}
+</style>
