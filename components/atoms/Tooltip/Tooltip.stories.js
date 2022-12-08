3eaba7996de7d6cae41825d0a20ed4be11eb895d
Nico <nico.hofmair@gmail.com>
2022-12-08 15:21:27 +0100

    PVWEB-824 | feature | docs

--- components/atoms/Tooltip/Tooltip.stories.js
+++ components/atoms/Tooltip/Tooltip.stories.js
@@ -8,13 +8,9 @@ export default {
       },
       source: {
         code: `
-        <div class="documentation-preview" style="position: relative; padding: 150px;">
-        <span v-tooltip.top-end="'Hello World'">Btn</span><br><br>
-        <span v-tooltip.top="'Very long text in this one... More and more text...'">Long text</span><br><br>
-        <span v-tooltip.top-end="{
-          content: '',
-        }">Show initially</span>
-      </div>
+<p v-tooltip.top-end="'Align right'">Align top-end, short text</p>
+<p v-tooltip.top="'This one falls back to display below because above is not enough space...'">Align top, long text</p>
+<p v-tooltip.top="">Dont show (empty content)</p>
 `,
       },
     },
@@ -29,7 +25,7 @@ const Template = (args) => ({
   <div class="documentation-preview" style="position: relative; padding: 120px; display: flex; justify-content: space-between;">
     <style>p { border: 1px solid black }</style>
     <p v-tooltip.top-end="'Align right'">Align top-end, short text</p>
-    <p v-tooltip.top="'This one falls back to display below because above is not enough space... Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio excepturi, accusantium molestiae veritatis repudiandae aperiam minus amet?'">Align top, long text</p>
+    <p v-tooltip.top="'This one falls back to display below because above is not enough space... Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio excepturi'">Align top, long text</p>
     <p v-tooltip.top="">Dont show (empty content)</p>
   </div>`,
 })
