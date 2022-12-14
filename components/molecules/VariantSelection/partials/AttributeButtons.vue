c423ee41f2ea456ab91dba8badfe4ea02fcd7e8a
Nico <nico.hofmair@gmail.com>
2022-12-14 13:39:07 +0100

    PVWEB-822 | refactore | use button component

--- components/molecules/VariantSelection/partials/AttributeButtons.vue
+++ components/molecules/VariantSelection/partials/AttributeButtons.vue
@@ -1,41 +1,23 @@
 <template>
-  <!-- accordion start -->
   <div class="attribute-buttons">
-    <label
-      v-for="(item, index) in values"
-      :key="index"
-      :class="[
-        'attribute-buttons__label',
-        {
-          'attribute-buttons__label--active': internalValue === item.value,
-        },
-      ]"
-    >
-      <input
-        v-bind="{ ...item, disabled }"
-        :checked="internalValue === item.value"
-        type="radio"
-        name="radio"
-        class="attribute-buttons__input"
-        @input="setActive(item.value)"
-      />
-      <span
-        :class="[
-          'attribute-buttons__text',
-          { 'attribute-buttons__text--disabled': disabled },
-        ]"
-      >
-        {{ item.value + ' ' + internalValue }}
-      </span>
-    </label>
+    <Button
+      v-for="item in values"
+      :key="item.value"
+      :label="item.displayValue"
+      :variant="item.selected ? 'secondary' : 'secondary'"
+      :shape="item.selected ? 'filled' : 'outlined'"
+      :disabled="!item.selectable"
+    />
   </div>
-  <!-- accordion end -->
 </template>
 <script>
-import { ref } from '@nuxtjs/composition-api'
+import { defineComponent, ref } from '@nuxtjs/composition-api'
+import Button from '~/components/atoms/Button/Button'
 
-export default {
-  name: 'AttributeButtons',
+export default defineComponent({
+  components: {
+    Button,
+  },
   props: {
     values: {
       type: Array,
@@ -61,7 +43,7 @@ export default {
 
     return { internalValue, setActive }
   },
-}
+})
 </script>
 <style lang="scss">
 .attribute-buttons {
@@ -70,48 +52,5 @@ export default {
   @apply tw-grid tw-grid-cols-3;
   @apply tw-gap-2;
   @apply tw-overflow-hidden;
-
-  &__label {
-    @apply tw-border-pv-grey-80;
-    @apply tw-border-2;
-    @apply tw-rounded-md;
-    @apply tw-py-3 tw-px-4;
-
-    &:hover {
-      @apply tw-cursor-pointer;
-      @apply tw-bg-pv-red-lighter;
-      @apply tw-border-pv-red-lighter;
-
-      #{$root}__text {
-        @apply tw-text-pv-white;
-      }
-    }
-  }
-
-  &__text {
-    @apply tw-inline-flex;
-    @apply tw-items-center;
-    @apply tw-justify-center;
-    @apply tw-h-full tw-w-full;
-    @apply tw-text-base;
-    @apply tw-leading-6;
-    @apply tw-font-bold;
-    @apply tw-text-pv-grey-16;
-
-    &--disabled {
-      @apply tw-text-pv-grey-80;
-    }
-  }
-
-  &__input {
-    @apply tw-hidden;
-
-    &:checked {
-      + span {
-        @apply tw-text-pv-white;
-        @apply tw-bg-pv-red;
-      }
-    }
-  }
 }
 </style>
