57a9def54582a5e6493677da9df0785352c75502
dhofmann <daniela.hofmann@diva-e.com>
2022-12-21 09:55:47 +0100

    PVWEB-824 | feat | add options obj to v-tooltip directive to enable trigger events

--- components/atoms/Tooltip/Tooltip.stories.js
+++ components/atoms/Tooltip/Tooltip.stories.js
@@ -23,10 +23,9 @@ const Template = (args) => ({
   },
   template: `
   <div class="documentation-preview" style="position: relative; padding: 120px; display: flex; justify-content: space-between;">
-    <style>p { border: 1px solid black }</style>
-    <p v-tooltip.top-end="'Align right'">Align top-end, short text</p>
-    <p v-tooltip.top="'This one falls back to display below because above is not enough space... Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio excepturi'">Align top, long text</p>
-    <p v-tooltip.top="">Dont show (empty content)</p>
+    <p v-tooltip="{ content: 'Align right', trigger: 'hover , click , focus', placement: 'top-end'}">Align top-end, short text</p>
+    <p v-tooltip="{ content: 'This one falls back to display below because above is not enough space... Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio excepturi', trigger: 'hover , click , focus', placement: 'top'}">Align top, long text</p>
+    <p v-tooltip="{ content: '', trigger: 'hover , click , focus', placement: 'top-end'}">Dont show (empty content)</p>
   </div>`,
 })
 
