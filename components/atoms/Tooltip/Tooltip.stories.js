a09c0245686ac29dfe4cd128df713a08ae5fa32a
Nico <nico.hofmair@gmail.com>
2022-12-23 09:36:29 +0100

    PVWEB-824 | refactore | use real composable

--- components/atoms/Tooltip/Tooltip.stories.js
+++ components/atoms/Tooltip/Tooltip.stories.js
@@ -1,3 +1,5 @@
+import { useTooltip } from '~/composables/useTooltip'
+
 export default {
   title: 'Technical components/Tooltip',
   parameters: {
@@ -8,48 +10,48 @@ export default {
       },
       source: {
         code: `
-<p v-tooltip.top-end="'Align right'">Align top-end, short text</p>
-<p v-tooltip.top="'This one falls back to display below because above is not enough space...'">Align top, long text</p>
-<p v-tooltip.top="">Dont show (empty content)</p>
-`,
+          const { getTooltip } = useTooltip()
+          const buttonTooltip1 = getTooltip({
+            content: 'default content',
+            trigger: 'hover',
+             placement: 'top-end',
+          })
+
+        <p v-tooltip="buttonToolTip1">Align top-end, short text</p>`,
       },
     },
   },
 }
 
-const getButtonTooltips = (trigger = 'hover') => [
-  {
-    content: 'default content',
-    trigger: trigger,
-    placement: 'top-end',
-  },
-  {
-    content:
-      'This one falls back to display below because above is not enough space... Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio excepturi',
-    trigger: trigger,
-    placement: 'top',
-  },
-  {
-    content: '',
-    trigger: trigger,
-    placement: 'top',
-  },
-]
-
-const Template = (args) => ({
+const Template = () => ({
   setup() {
-    return { args }
+    const { getTooltip } = useTooltip()
+    const buttonTooltip1 = getTooltip({
+      content: 'default content',
+      placement: 'top-end',
+    })
+
+    const buttonTooltip2 = getTooltip({
+      content:
+        'This one falls back to display below because above is not enough space... Lorem ipsum dolor sit amet consectetur Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum distinctio excepturi',
+      placement: 'top',
+    })
+
+    const buttonTooltip3 = getTooltip({
+      content: '',
+      placement: 'top',
+    })
+
+    console.log(buttonTooltip1)
+
+    return { buttonTooltip1, buttonTooltip2, buttonTooltip3 }
   },
   template: `
-  <div class="documentation-preview" style="position: relative; padding: 120px;">
-    <p v-tooltip="args[0]">Align top-end, short text</p>
-    <p v-tooltip="args[1]">Align top, long text</p>
-    <p v-tooltip="args[2]">Dont show (empty content)</p>
+  <div class="documentation-preview" style="position: relative; padding: 100px;">
+    <button v-tooltip="buttonTooltip1">Align top-end, short text</button><br><br>
+    <button v-tooltip="buttonTooltip2">Align top, long text</button><br><br>
+    <button v-tooltip="buttonTooltip3">Dont show (empty content)</button>
   </div>`,
 })
 
 export const Default = Template.bind({})
-Default.args = getButtonTooltips()
-
-export const MobileBehaviour = Template.bind({})
-MobileBehaviour.args = getButtonTooltips('click')
