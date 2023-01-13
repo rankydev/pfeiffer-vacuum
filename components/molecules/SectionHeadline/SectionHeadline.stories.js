cbadeaedabd393f7a4e5ff5f8c2323f8c744a0ed
philipp.timmel <philipp.timmel@diva-e.com>
2023-01-13 17:54:34 +0100

    PVWEB-497 | feat | Add Section Heading component

---
+++ components/molecules/SectionHeadline/SectionHeadline.stories.js
@@ -0,0 +1,39 @@
+import SectionHeadline from '~/components/molecules/SectionHeadline/SectionHeadline.vue'
+import {
+  headline,
+  buttons,
+  mobileLink,
+} from './SectionHeadline.stories.content'
+
+export default {
+  title: 'Molecules/m_124 SectionHeadline',
+  component: SectionHeadline,
+  parameters: {
+    docs: {
+      description: {
+        component:
+          'The Section Headline is used to device the my account dashboard sections. It can have buttons on the right side and a mobile link which is use for the arrow button on mobile devices instead of the desktop buttons.',
+      },
+      source: {
+        code: '<SectionHeadline v-bind="{ buttons, mobileLink }"> Headline </SectionHeadline>',
+      },
+    },
+  },
+}
+
+const Template = (args) => ({
+  components: { SectionHeadline },
+  setup() {
+    return { args, headline }
+  },
+  template: `
+  <div class="documentation-preview">
+  <SectionHeadline v-bind="args">
+    {{ headline }}
+  </SectionHeadline>
+  </div>
+`,
+})
+
+export const Default = Template.bind({})
+Default.args = { buttons, mobileLink }
---
+++ components/molecules/SectionHeadline/SectionHeadline.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="section-headline">
+    <h3 class="section-headline__text"><slot /></h3>
+    <div class="section-headline__buttons">
+      <Button
+        v-for="(btn, index) in buttons"
+        :key="index"
+        class="section-headline__buttons--desktop"
+        v-bind="{ ...btn }"
+      />
+      <Button
+        v-if="mobileLink"
+        icon="arrow_forward"
+        variant="secondary"
+        shape="plain"
+        class="section-headline__buttons--mobile"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SectionHeadline',
+  props: {
+    buttons: {
+      type: Array,
+      default: () => [],
+    },
+    mobileLink: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+<style lang="scss">
+.section-headline {
+  @apply tw-flex;
+  @apply tw-w-full;
+  @apply tw-justify-between;
+  @apply tw-items-center;
+
+  &__buttons {
+    @apply tw-flex;
+    @apply tw-gap-8;
+
+    &--desktop {
+      @apply tw-hidden;
+
+      @screen md {
+        @apply tw-flex;
+      }
+    }
+
+    &--mobile {
+      @apply tw-flex;
+
+      @screen md {
+        @apply tw-hidden;
+      }
+    }
+  }
+}
+</style>
