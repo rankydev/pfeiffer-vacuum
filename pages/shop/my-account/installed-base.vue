c52fcb670dcf84d1022c9ecfda0e7d14c1c0fcbb
Nico <nico.hofmair@gmail.com>
2023-01-16 18:46:14 +0100

    PVWEB-498 | feature | add page templates

---
+++ pages/shop/my-account/installed-base.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <MyAccountHeading />
+    <div
+      class="tw-flex tw-bg-pv-grey-88 tw-rounded-md tw-items-center tw-justify-center tw-font-bold tw-text-pv-white tw-text-2xl tw-text-center"
+      style="height: 800px"
+    >
+      <div>InstalledBase</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+import MyAccountHeading from '~/components/organisms/MyAccount/partials/MyAccountHeading'
+
+export default defineComponent({
+  name: 'InstalledBase',
+  components: {
+    MyAccountHeading,
+  },
+})
+</script>
