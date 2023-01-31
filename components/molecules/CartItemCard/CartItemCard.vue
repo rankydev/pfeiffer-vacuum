5bd700bc180dfe19fded853fa5cceec023872db0
Jessica Holst <>
2023-01-31 16:58:26 +0100

    PVWEB-481 | feat | styling responsive + minicart possible

--- components/molecules/CartItemCard/CartItemCard.vue
+++ components/molecules/CartItemCard/CartItemCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     v-if="entry.product"
+    class="cart-item-card"
     :class="[
       isMinicart
         ? 'cart-item-card--minicart tw-bg-pv-white'
@@ -8,33 +9,42 @@
     ]"
   >
     <div class="cart-item-card__content-wrapper">
-      <div class="cart-item-card___content-wrapper--first-row">
-        <div class="cart-item-card___product">
-          <div class="cart-item-card__image">
-            <!-- <Link :href="getUrl"> -->
-            <Link :href="'#'">
-              <ResponsiveImage
-                class="cart-item-card__product-image"
-                :image="entry.product.images[0]"
-                provider="hybris"
-                aspect-ratio="1:1"
-              />
-            </Link>
-          </div>
-          <div class="cart-item-card__product-code">
-            <h6>
+      <div class="cart-item-card__content-wrapper--first-row">
+        <div class="cart-item-card__product-delete">
+          <div class="cart-item-card__product">
+            <div class="cart-item-card__image">
               <!-- <Link :href="getUrl"> -->
               <Link :href="'#'">
-                {{ entry.product.name }}
+                <ResponsiveImage
+                  class="cart-item-card__product-image"
+                  :image="entry.product.images[0]"
+                  provider="hybris"
+                  aspect-ratio="1:1"
+                />
               </Link>
-            </h6>
-            <p class="cart-item-card__product-order-number">
-              {{ entry.product.orderNumber }}
-            </p>
+            </div>
+            <div class="cart-item-card__product-code">
+              <h6>
+                <!-- <Link :href="getUrl"> -->
+                <Link :href="'#'">
+                  {{ entry.product.name }}
+                </Link>
+              </h6>
+              <p class="cart-item-card__product-order-number">
+                {{ entry.product.orderNumber }}
+              </p>
+            </div>
+          </div>
+          <div
+            class="cart-item-card__delete-wrapper cart-item-card__delete-mobile"
+          >
+            <div class="cart-item-card__delete">
+              <Button variant="secondary" shape="plain" icon="delete" />
+            </div>
           </div>
         </div>
         <div class="cart-item-card__quantity-price">
-          <div class="cart-item-card__quanity">
+          <div class="cart-item-card__quantity">
             <span v-if="readOnly">
               {{ $t('cart.quantity') }}: {{ entry.quantity }}
             </span>
@@ -42,20 +52,24 @@
               v-else
               v-model="quantity"
               input-type="number"
-              class="cart-item-card__quanity-input"
+              class="cart-item-card__quantity-input"
               @input="updateQuantity"
             />
           </div>
           <div class="cart-item-card__prices">
-            <p>{{ productPrice }}</p>
-            <p v-if="productPrice">{{ `${totalPrice}` }}</p>
-            <p v-else>{{ totalPrice }}</p>
+            <div class="cart-item-card__prices--single-price">
+              <span class="cart-item-card__prices--show-headline">Price</span>
+              <p>{{ ` € ${productPrice}` }}</p>
+            </div>
+            <div class="cart-item-card__prices--total-price">
+              <span class="cart-item-card__prices--show-headline">Total</span>
+              <p v-if="productPrice">{{ ` € ${totalPrice}` }}</p>
+              <p v-else>{{ totalPrice }}</p>
+            </div>
           </div>
         </div>
       </div>
-      <div
-        class="cart-item-card___content-wrapper--second-row tw-flex tw-flex-col"
-      >
+      <div class="cart-item-card___content-wrapper--second-row">
         <div class="cart-item-card__attribute-wrapper">
           <div
             v-if="showAttributes"
@@ -195,7 +209,7 @@
         </div>
       </div>
     </div>
-    <div class="cart-item-card__delete-wrapper">
+    <div class="cart-item-card__delete-wrapper cart-item-card__delete-desktop">
       <div class="cart-item-card__delete">
         <Button variant="secondary" shape="plain" icon="delete" />
       </div>
@@ -239,7 +253,7 @@ export default defineComponent({
     },
     isMinicart: {
       type: Boolean,
-      default: true,
+      default: false,
     },
   },
   // emits: ['input'],
@@ -345,10 +359,154 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.cart-item-card--minicart {
-  @apply tw-flex tw-flex-row;
+.cart-item-card {
   @apply tw-py-6;
   @apply tw-px-2;
+
+  &__content-wrapper {
+    &--first-row {
+      @apply tw-flex-row;
+    }
+
+    &--second-row {
+      @apply tw-flex tw-flex-col;
+    }
+  }
+
+  &__quantity-price {
+    @apply tw-flex;
+    @apply tw-justify-between;
+    @apply tw-mt-4;
+
+    @screen lg {
+      @apply tw-ml-2;
+      @apply tw-mt-0;
+    }
+  }
+
+  &__quantity {
+    max-width: 200px;
+
+    @screen lg {
+      @apply tw-mr-12;
+      max-width: 64px;
+    }
+  }
+
+  &__image {
+    @apply tw-inline-flex;
+    @apply tw-justify-center;
+    @apply tw-items-center;
+    @apply tw-mr-4;
+    max-width: 48px;
+  }
+
+  &__prices {
+    @apply tw-flex;
+    @apply tw-flex-col;
+    @apply tw-justify-end;
+    @apply tw-items-center;
+
+    @screen md {
+      @apply tw-flex-row;
+    }
+
+    &--show-headline {
+      display: block;
+
+      @screen lg {
+        @apply tw-hidden;
+      }
+    }
+
+    &--single-price {
+      @apply tw-flex;
+      @apply tw-items-center;
+
+      span {
+        @apply tw-mr-2;
+        @apply tw-text-xs;
+        @apply tw-text-pv-grey-48;
+      }
+
+      p {
+        @apply tw-text-base;
+
+        @screen lg {
+          @apply tw-text-xl;
+        }
+      }
+
+      @screen md {
+        @apply tw-mr-12;
+      }
+    }
+
+    &--total-price {
+      @apply tw-flex;
+      @apply tw-items-center;
+
+      span {
+        @apply tw-mr-2;
+        @apply tw-text-xs;
+        @apply tw-text-pv-grey-48;
+      }
+
+      p {
+        @apply tw-font-bold;
+        @apply tw-text-base;
+
+        @screen lg {
+          @apply tw-font-normal;
+          @apply tw-text-xl;
+        }
+      }
+    }
+  }
+
+  &__variation-attributes-row {
+    @apply tw-flex tw-flex-wrap;
+  }
+
+  &__attribute {
+    @apply tw-flex;
+    @apply tw-mb-2;
+  }
+
+  &__attribute-value {
+    @apply tw-bg-pv-grey-96;
+    @apply tw-text-xs;
+    @apply tw-px-2;
+    @apply tw-py-1;
+    @apply tw-mt-2;
+    @apply tw-mr-2;
+    @apply tw-rounded-md;
+  }
+
+  &__attribute-name {
+    @apply tw-text-pv-grey-64;
+  }
+
+  &__promotions {
+    @apply tw-flex;
+    @apply tw-flex-wrap;
+  }
+
+  &__promotion {
+    @apply tw-bg-pv-grey-16;
+    @apply tw-text-pv-white;
+    @apply tw-rounded-md;
+    @apply tw-font-bold;
+    @apply tw-text-sm;
+    @apply tw-px-2;
+    @apply tw-py-1;
+    @apply tw-mt-2;
+    @apply tw-mr-2;
+  }
+}
+
+.cart-item-card--minicart {
+  @apply tw-flex tw-flex-row;
 
   .cart-item-card {
     &__content-wrapper {
@@ -359,175 +517,97 @@ export default defineComponent({
       }
     }
 
-    &__product {
+    &__delete-mobile {
       @apply tw-flex;
     }
 
-    &__image {
-      max-width: 48px;
+    &__delete-desktop {
+      @apply tw-hidden;
     }
 
-    &__quantity-price {
+    &__product-delete {
       @apply tw-flex;
       @apply tw-justify-between;
     }
 
+    &__product {
+      @apply tw-flex;
+    }
+
     &__prices {
-      @apply tw-flex;
-      @apply tw-gap-1;
       @apply tw-items-center;
-    }
-
-    &__variation-attributes-row {
-      @apply tw-flex tw-flex-wrap;
-    }
-
-    &__attribute-value {
-      @apply tw-bg-pv-grey-96;
-      @apply tw-text-xs;
-      @apply tw-px-2;
-      @apply tw-py-1;
-      @apply tw-mt-2;
-      @apply tw-mr-2;
-      @apply tw-rounded-md;
-    }
-
-    &__promotions {
-      @apply tw-flex;
-      @apply tw-flex-wrap;
-    }
-
-    &__promotion {
-      @apply tw-bg-pv-grey-16;
-      @apply tw-text-pv-white;
-      @apply tw-rounded-md;
-      @apply tw-font-bold;
-      @apply tw-text-sm;
-      @apply tw-px-2;
-      @apply tw-py-1;
-      @apply tw-mt-2;
-      @apply tw-mr-2;
     }
   }
 }
 
 .cart-item-card--desktop {
+  background-color: white;
+
+  @screen lg {
+    @apply tw-flex;
+    @apply tw-justify-between;
+  }
+
   .cart-item-card {
-    @apply tw-container;
-    @apply tw-py-6;
+    &__content-wrapper {
+      &--first-row {
+        @apply tw-flex;
+        @apply tw-flex-col;
 
-    &__wrapper {
-      @apply tw-grid tw-grid-cols-12;
+        @screen lg {
+          @apply tw-flex-row;
+          @apply tw-justify-between;
+        }
+      }
     }
 
-    &__image {
-      @apply tw-col-span-1;
-      @apply tw-inline-flex;
-      @apply tw-justify-center;
-      @apply tw-items-center;
-      @apply tw-mr-4;
-      max-height: 48px;
-
-      // md:tw-col-span-1 tw-pr-3
+    &__product-delete {
+      @apply tw-flex;
+      @apply tw-justify-between;
     }
 
-    &__product-code {
-      @apply tw-col-span-6;
-
-      // md:tw-col-span-6 tw-pl-3
+    &__product {
+      @apply tw-flex;
     }
 
     &__quantity {
-      @apply tw-col-span-1;
-
-      // md:tw-col-span-4 lg:tw-col-span-1 tw-pr-3
+      @screen lg {
+        width: 64px;
+      }
     }
 
     &__prices {
-      @apply tw-col-span-3;
-      @apply tw-flex;
-      @apply tw-justify-evenly;
-      @apply tw-items-center;
+      @screen lg {
+        width: 85%;
+      }
     }
 
-    &__price-row {
-      @apply tw-grid tw-grid-cols-12;
+    &__delete-mobile {
+      @apply tw-flex;
+
+      @screen lg {
+        @apply tw-hidden;
+      }
     }
 
-    &__delete {
-      @apply tw-col-span-1;
-      @apply tw-flex;
-      @apply tw-justify-end;
+    &__delete-desktop {
+      @apply tw-hidden;
+
+      @screen lg {
+        @apply tw-flex;
+      }
     }
 
     &__attribute-wrapper {
-      @apply tw-grid;
-      @apply tw-grid-cols-12;
       @apply tw-mt-4;
     }
 
-    &__variation-attributes-row {
-      @apply tw-col-start-2 tw-col-span-6;
-      @apply tw-flex;
-      @apply tw-flex-wrap;
-    }
-
-    &__cart-item-card__variation-attributes {
-      @apply tw-flex;
-    }
-
-    &__attribute {
-      @apply tw-flex;
-      @apply tw-mb-2;
-    }
-
-    &__attribute-value {
-      @apply tw-bg-pv-grey-96;
-      @apply tw-text-xs;
-      @apply tw-px-2;
-      @apply tw-py-1;
-      @apply tw-mr-2;
-      @apply tw-rounded-md;
-    }
-
-    &__attribute-name {
-      @apply tw-text-pv-grey-64;
-    }
-
-    &__promotions-wrapper {
-      @apply tw-grid;
-      @apply tw-grid-cols-12;
-    }
-
-    &__promotions {
-      @apply tw-col-span-6;
-      @apply tw-col-start-2;
-      @apply tw-flex;
-      @apply tw-flex-wrap;
-    }
-
-    &__promotion {
-      @apply tw-bg-pv-grey-16;
-      @apply tw-text-pv-white;
-      @apply tw-rounded-md;
-      @apply tw-font-bold;
-      @apply tw-text-sm;
-      @apply tw-px-2;
-      @apply tw-py-1;
-      @apply tw-mt-2;
-      @apply tw-mr-2;
-    }
-
     &__further-article-information {
-      @apply tw-grid;
-      @apply tw-grid-cols-12;
       @apply tw-mt-6;
     }
 
     &__add-to-other-list {
       @apply tw-flex;
-      @apply tw-col-span-2;
-      @apply tw-col-start-2;
     }
   }
 }
