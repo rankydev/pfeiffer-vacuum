74190aa9f4102b9eae79779dc28660f58c5766a5
Dennis Schuchardt <schuchardt.dennis@gmail.com>
2023-02-07 21:20:11 +0100

    cart item card v1 with header

--- components/molecules/CartItemCard/CartItemCard2.vue
+++ components/molecules/CartItemCard/CartItemCard2.vue
@@ -10,13 +10,15 @@
       </span>
     </div>
     <div v-for="i in 6" :key="i" class="cart-item-card">
-      <Link :href="url" class="cart-item-card-image">
-        <ResponsiveImage
-          :image="entry.product.images[0]"
-          provider="hybris"
-          aspect-ratio="1:1"
-        />
-      </Link>
+      <div class="cart-item-card-image">
+        <Link :href="url">
+          <ResponsiveImage
+            :image="entry.product.images[0]"
+            provider="hybris"
+            aspect-ratio="1:1"
+          />
+        </Link>
+      </div>
       <div class="cart-item-card-title">
         <h2 class="cart-item-card-title__mainTitle">
           <Link :href="url">{{ entry.product.name }}</Link>
@@ -39,13 +41,17 @@
         <span class="cart-item-card-price__label">
           {{ $t('cart.productPrice') }}
         </span>
-        <span>{{ ` € ${productPrice}` }}</span>
+        <span class="cart-item-card-price__price">{{
+          `€ ${productPrice}`
+        }}</span>
       </div>
       <div class="cart-item-card-totalPrice">
-        <span class="cart-item-card-totalPrice__label">{{
-          $t('cart.totalPrice')
-        }}</span>
-        <span>{{ ` € ${totalPrice}` }}</span>
+        <span class="cart-item-card-totalPrice__label">
+          {{ $t('cart.totalPrice') }}
+        </span>
+        <span class="cart-item-card-totalPrice__price">
+          {{ `€ ${totalPrice}` }}
+        </span>
       </div>
       <Button
         class="cart-item-card-addArticle"
@@ -183,36 +189,47 @@ export default defineComponent({
 }
 
 .cart-item-card {
-  @apply tw-grid tw-grid-rows-5 tw-grid-cols-12;
+  @apply tw-grid tw-grid-cols-12 tw-auto-rows-auto;
   @apply tw-border-b tw-border-b-pv-grey-80;
   @apply tw-mt-6;
+  @apply tw-mx-4;
 
   @screen lg {
     @apply tw-grid-rows-3;
   }
 
   &-image {
-    @apply tw-row-start-1 tw-row-end-1;
-    @apply tw-col-start-1 tw-col-end-1;
+    @apply tw-row-start-1 tw-row-end-2;
+    @apply tw-col-start-1 tw-col-end-3;
+    @apply tw-flex;
 
-    @screen lg {
-      @apply tw-row-start-1 tw-row-end-3;
+    @screen md {
+      @apply tw-col-end-2;
     }
   }
 
   &-title {
     @apply tw-row-start-1 tw-row-end-1;
-    @apply tw-col-start-2 tw-col-end-11;
-    @apply tw-text-base;
-    @apply tw-leading-6;
+    @apply tw-col-start-4 tw-col-end-12;
+    @apply tw-flex tw-flex-col;
+    @apply tw-ml-4;
+
+    @screen md {
+      @apply tw-col-start-2;
+    }
 
     @screen lg {
+      @apply tw-ml-0;
       @apply tw-col-start-2 tw-col-end-8;
     }
 
     &__mainTitle {
+      @apply tw-text-base;
+      @apply tw-leading-6;
+
       @screen lg {
         @apply tw-text-lg;
+        @apply tw-leading-7;
       }
     }
 
@@ -224,82 +241,129 @@ export default defineComponent({
 
   &-quantity {
     @apply tw-row-start-2 tw-row-end-3;
-    @apply tw-col-start-1 tw-col-end-6;
+    @apply tw-col-start-1 tw-col-end-7;
+    @apply tw-mt-4;
+    @apply tw-flex;
 
     @screen lg {
       @apply tw-row-start-1 tw-row-end-1;
-      @apply tw-col-start-9 tw-col-end-9;
+      @apply tw-col-start-9 tw-col-end-10;
       @apply tw-w-20;
+      @apply tw-m-auto;
     }
   }
 
   &-price {
-    @apply tw-row-start-2 tw-row-end-2;
-    @apply tw-col-start-7 tw-col-end-12;
+    @apply tw-row-start-2 tw-row-end-3;
+    @apply tw-col-start-7 tw-col-end-13;
+    @apply tw-leading-6;
+    @apply tw-flex;
+    @apply tw-ml-auto;
+    @apply tw-mt-4;
+
+    @screen md {
+      @apply tw-my-auto;
+      @apply tw-col-start-9 tw-col-end-11;
+    }
 
     @screen lg {
       @apply tw-row-start-1 tw-row-end-1;
-      @apply tw-col-start-10 tw-col-end-10;
+      @apply tw-col-start-10 tw-col-end-11;
       @apply tw-text-lg;
+      @apply tw-leading-7;
     }
 
     &__label {
-      @apply tw-text-sm;
-      @apply tw-leading-6;
+      @apply tw-text-xs;
+      @apply tw-ml-2;
 
       @screen lg {
         @apply tw-hidden;
       }
     }
+
+    &__price {
+      @apply tw-ml-2;
+    }
   }
 
   &-totalPrice {
-    @apply tw-row-start-2 tw-row-end-2;
-    @apply tw-col-start-7 tw-col-end-12;
-    @apply tw-text-sm;
+    @apply tw-row-start-2 tw-row-end-3;
+    @apply tw-col-start-7 tw-col-end-13;
     @apply tw-leading-6;
-    @apply tw-mt-16;
+    @apply tw-flex;
+    @apply tw-mt-auto;
+    @apply tw-ml-auto;
 
     @screen md {
-      @apply tw-row-start-2 tw-row-end-2;
-      @apply tw-col-start-11 tw-col-end-11;
+      @apply tw-mb-auto;
+      @apply tw-col-start-11 tw-col-end-13;
     }
 
     @screen lg {
-      @apply tw-row-start-1 tw-row-end-1;
-      @apply tw-text-lg;
+      @apply tw-row-start-1 tw-row-end-2;
+      @apply tw-col-start-11 tw-col-end-12;
     }
 
     &__label {
+      @apply tw-text-xs;
+
       @screen lg {
         @apply tw-hidden;
+      }
+    }
+
+    &__price {
+      @apply tw-text-base;
+      @apply tw-font-bold;
+      @apply tw-ml-2;
+
+      @screen lg {
+        @apply tw-text-lg;
+        @apply tw-leading-7;
+        @apply tw-font-normal;
       }
     }
   }
 
   &-delete {
-    @apply tw-row-start-1 tw-row-end-1;
-    @apply tw-col-start-12 tw-col-end-12;
+    @apply tw-row-start-1 tw-row-end-2;
+    @apply tw-col-start-12 tw-col-end-13;
+    @apply tw-mb-auto;
+    @apply tw-ml-auto;
+    padding: 0 0 0 8px !important;
+
+    @screen lg {
+      @apply tw-mt-auto;
+      @apply tw-mr-auto;
+    }
   }
 
   &-tags {
     @apply tw-row-start-3 tw-row-end-4;
     @apply tw-col-start-1 tw-col-end-12;
+    @apply tw-mt-4;
 
     @screen lg {
-      @apply tw-row-start-2 tw-row-end-2;
+      @apply tw-row-start-2 tw-row-end-3;
       @apply tw-col-start-2 tw-col-end-8;
     }
   }
 
   &-addArticle {
-    @apply tw-row-start-5 tw-row-end-5;
-    @apply tw-col-start-1 tw-col-end-12;
+    @apply tw-row-start-5 tw-row-end-6;
+    @apply tw-col-start-1 tw-col-end-13;
     @apply tw-w-fit;
+    @apply tw-mx-auto;
+    @apply tw-mb-3;
+
+    @screen md {
+      @apply tw-mx-0;
+    }
 
     @screen lg {
-      @apply tw-row-start-3 tw-row-end-3;
-      @apply tw-col-start-2 tw-col-end-12;
+      @apply tw-row-start-3 tw-row-end-4;
+      @apply tw-col-start-2 tw-col-end-13;
     }
   }
 }
