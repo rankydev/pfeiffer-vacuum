f5e9095b1a2ef2ef80d0f3c1e6fe1a7914aa9e0d
Nico <nico.hofmair@gmail.com>
2023-02-08 18:26:23 +0100

    PVWEB-550 | feat | initial store setup for filters

--- stores/empolis/empolis.js
+++ stores/empolis/empolis.js
@@ -1,5 +1,5 @@
 import { defineStore } from 'pinia'
-import { ref, useContext, useRoute } from '@nuxtjs/composition-api'
+import { computed, ref, useContext, useRoute } from '@nuxtjs/composition-api'
 import { useAxiosForEmpolis } from '~/composables/useAxiosForEmpolis'
 import { useLanguageSwitch } from '~/composables/useLanguageSwitch'
 import { useLogger } from '~/composables/useLogger'
@@ -20,44 +20,43 @@ export const useEmpolisStore = defineStore('empolis', () => {
   const { i18n } = useContext()
   const { onLanguageSwitched } = useLanguageSwitch()
 
-  const getProductDownloads = async (orderNumber) => {
-    if (!productDownloads.value.has(orderNumber)) {
-      let results = []
-      const files = await axios.$post('/search', {
-        filter: {
-          ProductID: [orderNumber],
-        },
-        limit: PDP_DOCUMENT_LIMIT,
-        language: i18n.locale === 'de' ? 'de' : 'en',
-      })
-
-      if (files && Array.isArray(files.results) && !files.error) {
-        results = files.results
-      } else {
-        logger.error(
-          `Error when fetching product downloads '${orderNumber}'. Returning empty array.`,
-          files.error
-        )
-      }
-      productDownloads.value.set(orderNumber, results)
-    }
-    return productDownloads.value.get(orderNumber)
-  }
+  const language = computed(() => {
+    return i18n.locale === 'de' ? 'de' : 'en'
+  })
 
   const loadByPath = async () => {
     // first check if we have cached results for this url (includes all filters, pageSize, ...)
     const cacheKey = route.value.fullPath
-    if (searchResultsCache.value.has(cacheKey)) {
-      searchResults.value = searchResultsCache.value.get(cacheKey)
-      return
-    }
+    // TODO: this needs to be rethinked because multiple request results need to cached (filters, translated filters, )
+    // if (searchResultsCache.value.has(cacheKey)) {
+    //   searchResults.value = searchResultsCache.value.get(cacheKey)
+    //   return
+    // }
 
     // load search results by query if not found in cache
     const term = route.value.query.searchTerm || ''
     const currentPage = Number(route.value.query.currentPage || 1)
     const pageSize = Number(route.value.query.pageSize || PAGE_SIZE_DEFAULT)
 
+    const filtersFromQuery = route.value.query.filter
+      ? JSON.parse(decodeURIComponent(route.value.query.filter.toString()))
+      : {}
+
     try {
+      // first get translates filters filters
+      const activeFilters = await getTranslatedFilters(filtersFromQuery)
+      console.log('activeFilters', activeFilters)
+
+      // then fetch all available filters dependent on the current items
+      const availableFilters = await getFilterItems(
+        filtersFromQuery,
+        activeFilters
+      )
+      console.log('availableFilters', availableFilters)
+
+      // TODO: also load filterSuggestions with "annotateText". Can be async (without await in the first place for parallelization)
+      // no dependency after it as it looks like now
+
       // TODO: grep filter options here later, too. [PVWEB-550]
       const files = await fetchSearchResults({
         text: term,
@@ -71,6 +70,67 @@ export const useEmpolisStore = defineStore('empolis', () => {
     }
   }
 
+  const getTranslatedFilters = async (filters) => {
+    try {
+      const result = await axios.$post('/translate', {
+        filters,
+        language: language.value,
+      })
+
+      if (result && Array.isArray(result) && !result.error) {
+        return JSON.parse(JSON.stringify(result))
+      }
+      throw result.error ? result.error : 'unknown error'
+    } catch (e) {
+      logger.error(
+        'Error when translating filterObject. Returning empty array.',
+        e ? e : ''
+      )
+      return []
+    }
+  }
+
+  const getFilterItems = async (filters, activeFilters) => {
+    try {
+      const result = await axios.$post('/filters', {
+        filter: filters, // NOTE: yes this is no typo but the API really needs "filter" here instead of "filters"
+        language: language.value,
+      })
+
+      if (result && Array.isArray(result.filters) && !result.error) {
+        // 'DocumentType' and 'Category' are standardized Empolis filters we don't want to display
+        const tempFilters = result.filters.filter(
+          (obj) => !['DocumentType', 'Category'].includes(obj.attribute)
+        )
+
+        const activeFiltersString = []
+        for (let i = 0; i < activeFilters.length; i++) {
+          for (let k = 0; k < activeFilters[i].concepts.length; k++) {
+            activeFiltersString.push(activeFilters[i].concepts[k].id)
+          }
+        }
+
+        for (let i = 0; i < tempFilters.length; i++) {
+          for (let k = 0; k < tempFilters[i].concepts.length; k++) {
+            tempFilters[i].concepts[k].checked = activeFiltersString.includes(
+              tempFilters[i].concepts[k].id
+            )
+          }
+        }
+
+        return tempFilters.map((v) => ({ ...v, visible: true }))
+      }
+
+      throw result.error ? result.error : 'unknown error'
+    } catch (e) {
+      logger.error(
+        'Error when fetching filters. Returning empty array.',
+        e ? e : ''
+      )
+      return []
+    }
+  }
+
   const fetchSearchResults = async (searchOptions = {}) => {
     // composing final search options by overriding defaults with (optional) custom options
     const options = {
@@ -79,7 +139,7 @@ export const useEmpolisStore = defineStore('empolis', () => {
       offset: 0,
       limit: PAGE_SIZE_DEFAULT,
       ...searchOptions,
-      language: i18n.locale === 'de' ? 'de' : 'en',
+      language: language.value,
     }
 
     searchResultsLoading.value = true
@@ -112,6 +172,30 @@ export const useEmpolisStore = defineStore('empolis', () => {
     } finally {
       searchResultsLoading.value = false
     }
+  }
+
+  const getProductDownloads = async (orderNumber) => {
+    if (!productDownloads.value.has(orderNumber)) {
+      let results = []
+      const files = await axios.$post('/search', {
+        filter: {
+          ProductID: [orderNumber],
+        },
+        limit: PDP_DOCUMENT_LIMIT,
+        language: language.value,
+      })
+
+      if (files && Array.isArray(files.results) && !files.error) {
+        results = files.results
+      } else {
+        logger.error(
+          `Error when fetching product downloads '${orderNumber}'. Returning empty array.`,
+          files.error
+        )
+      }
+      productDownloads.value.set(orderNumber, results)
+    }
+    return productDownloads.value.get(orderNumber)
   }
 
   onLanguageSwitched(() => {
