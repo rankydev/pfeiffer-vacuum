24e9fe05d81960fe867f7bd12c94f44dc4f84ced
Dennis Schuchardt <schuchardt.dennis@gmail.com>
2023-02-14 11:31:50 +0100

    save 14022023

---
+++ components/molecules/CartOverlay/CartOverlay.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="cart-overlay">
+    <div class="cart-overlay-title">
+      <h2>
+        <span>$t('cart.shoppingCart')</span>
+        <span>({{ numberOfDifferentProducts }})</span>
+      </h2>
+    </div>
+    <CartTable :cart="cart" :is-mini-cart="isMiniCart" />
+  </div>
+</template>
+
+<script>
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  ref,
+} from '@nuxtjs/composition-api'
+import CartTable from '../CartTable/CartTable.vue'
+
+export default defineComponent({
+  name: 'CartOverlay',
+  components: {
+    CartTable,
+  },
+  setup() {
+    const cart = ref([])
+    const isMiniCart = ref(true)
+
+    const numberOfDifferentProducts = computed(() => {
+      return cart.value.products.length
+    })
+    const initCartFromStore = () => {
+      console.log('initCartfromStore')
+    }
+    onMounted(() => {
+      initCartFromStore()
+    })
+
+    return { cart, isMiniCart, numberOfDifferentProducts }
+  },
+})
+</script>
