70fd291257b723744dea1d1003bc95844a3ae246
Henrik Pilz <henrik.pilz@diva-e.com>
2023-02-16 17:07:31 +0100

    PVWEB-945 | feat | adapt hybris proxies to use runtimeConfig

--- server/middleware/shop-images.js
+++ server/middleware/shop-images.js
@@ -1,18 +1,18 @@
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import https from 'https'
 import { PATH_SHOP_IMAGES } from '../constants.js'
-import config from '../../nuxt.config'
+import nuxtConfig from '../../nuxt.config'
 
 const agent = new https.Agent({
   keepAlive: true,
   rejectUnauthorized: process.env.IGNORE_SSL_CERT !== 'true',
 })
 
-const regionCodes = config.publicRuntimeConfig.REGION_CODES
+const regionCodes = nuxtConfig.publicRuntimeConfig.REGION_CODES
 const regionsForRegex = regionCodes.replaceAll(',', '|')
 
 export default createProxyMiddleware({
-  target: process.env.SHOP_IMAGE_URL,
+  target: nuxtConfig.privateRuntimeConfig.SHOP_IMAGE_URL,
   changeOrigin: true,
   agent,
   pathRewrite: { [`^(/(${regionsForRegex}))?${PATH_SHOP_IMAGES}`]: '/' },
