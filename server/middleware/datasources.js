d0f5c18719fda881ee9437cbe588faed94f64113
Henrik Pilz <henrik.pilz@diva-e.com>
2023-02-16 17:05:59 +0100

    PVWEB-945 | feat | move STORYBLOK_ACCESS_TOKEN to proxy

--- server/middleware/datasources.js
+++ server/middleware/datasources.js
@@ -1,6 +1,6 @@
 import { createProxyMiddleware } from 'http-proxy-middleware'
 import https from 'https'
-import config from '../../nuxt.config'
+import nuxtConfig from '../../nuxt.config'
 import { PATH_DATASOURCES } from '../constants.js'
 
 const agent = new https.Agent({
@@ -8,12 +8,32 @@ const agent = new https.Agent({
   rejectUnauthorized: process.env.IGNORE_SSL_CERT !== 'true',
 })
 
-const regionCodes = config.publicRuntimeConfig.REGION_CODES
+const regionCodes = nuxtConfig.publicRuntimeConfig.REGION_CODES
 const regionsForRegex = regionCodes.replaceAll(',', '|')
 
 export default createProxyMiddleware({
-  target: `${process.env.STORYBLOK_API_BASE_URL}`,
+  target: `${nuxtConfig.privateRuntimeConfig.STORYBLOK_API_BASE_URL}`,
   changeOrigin: true,
   agent,
-  pathRewrite: { [`^(/(${regionsForRegex}))${PATH_DATASOURCES}`]: '/' },
+  pathRewrite: (path, req) => {
+    let newPath = path.replace(
+      `^(/(${regionsForRegex}))${PATH_DATASOURCES}`,
+      '/'
+    )
+
+    const newQuery = { ...req.query } // copy object
+    if (Object.keys(newQuery).length) {
+      // There were more query parameters than just _csrf
+      newPath = `${newPath.split('?')[0]}?${querystring.stringify(
+        newQuery
+      )}&token=${nuxtConfig.privateRuntimeConfig.STORYBLOK_ACCESS_TOKEN}`
+    } else {
+      // _csrf was the only query parameter
+      newPath = `${newPath.split('?')[0]}?token=${
+        nuxtConfig.privateRuntimeConfig.STORYBLOK_ACCESS_TOKEN
+      }`
+    }
+    console.log('newPath: ', newPath)
+    return newPath
+  },
 })
