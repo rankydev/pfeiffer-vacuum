a232fedc2663eddc771918334fbcfc112259eb01
Jessica Holst <>
2023-02-22 15:24:07 +0100

    PVWEB-567 | feat | added styles to component

--- components/molecules/KnowledgeAuthor/KnowledgeAuthor.vue
+++ components/molecules/KnowledgeAuthor/KnowledgeAuthor.vue
@@ -1,7 +1,12 @@
 <template>
   <li class="knowledge-author">
-    <img :src="image.filename" />
-    <ResponsiveImage style="width: 80px" :image="image" aspect-ratio="1:1" />
+    <!-- <img :src="image" /> -->
+    <ResponsiveImage
+      class="knowledge-author__image"
+      :image="image"
+      aspect-ratio="1:1"
+      :rounded="true"
+    />
     <p class="knowledge-author__description">
       <span class="knowledge-author__name">{{ name }}</span>
       <span class="knowledge-author__title">{{ title }}</span>
@@ -10,8 +15,11 @@
 </template>
 
 <script>
-export default {
+import { defineComponent } from '@nuxtjs/composition-api'
+import ResponsiveImage from '~/components/atoms/ResponsiveImage/ResponsiveImage'
+export default defineComponent({
   name: 'KnowledgeAuthor',
+  components: { ResponsiveImage },
   props: {
     image: {
       type: Object,
@@ -27,20 +35,20 @@ export default {
       required: true,
     },
   },
-}
+})
 </script>
 
 <style lang="scss">
 .knowledge-author {
   @apply tw-flex;
   @apply tw-mb-4;
+  @apply tw-pr-4;
+  @apply tw-bg-pv-grey-96;
 
-  img {
+  &__image {
     @apply tw-mr-4;
-    width: 72px;
-    height: 72px;
-    object-fit: cover;
-    border-radius: 50%;
+    width: 80px;
+    height: 80px;
   }
 
   &__description {
@@ -51,10 +59,20 @@ export default {
 
   &__name {
     @apply tw-font-bold;
+    @apply tw-text-base;
+
+    @screen lg {
+      @apply tw-text-xl;
+    }
   }
 
   &__title {
     @apply tw-text-pv-grey-64;
+    @apply tw-text-sm;
+
+    @screen lg {
+      @apply tw-text-base;
+    }
   }
 }
 </style>
