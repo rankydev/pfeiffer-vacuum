a1e294bbe33eeba514b1e7a14dc0f2423c4019da
julius.schultz <julius.schultz@diva-e.com>
2023-02-23 10:17:38 +0100

    PVWEB-491 | feat | migration of pvac component

--- components/organisms/Confirmation/Confirmation.vue
+++ components/organisms/Confirmation/Confirmation.vue
@@ -1,125 +1,129 @@
 <template>
-  <div class="registration-success-page">
-    <div class="registration-success-page--content-wrapper">
-      <Icon class="registration-success-page__icon" icon="check_circle" />
-      <h1 class="registration-success-page__title">
-        {{
-          $t(
-            `registration.registrationSuccessPage.${
-              isLiteRegistration ? 'lite' : 'full'
-            }.title`
-          )
-        }}
-      </h1>
-      <p class="registration-success-page__paragraph">
-        {{
-          $t(
-            `registration.registrationSuccessPage.${
-              isLiteRegistration ? 'lite' : 'full'
-            }.paragraph`
-          )
-        }}
-      </p>
+  <div class="confirmation">
+    <div class="confirmation__headline">
+      <h3 class="lg:tw-hidden">
+        {{ $t('checkout.requestSuccess') }}
+      </h3>
+      <h2 class="tw-hidden lg:tw-block">
+        {{ $t('checkout.requestSuccess') }}
+      </h2>
+    </div>
+    <div class="confirmation__description">
+      <h6 class="sub md:tw-hidden">
+        {{ $t('checkout.requestDescription') }}
+      </h6>
+      <h5 class="sub tw-hidden md:tw-block">
+        {{ $t('checkout.requestDescription') }}
+      </h5>
+    </div>
+    <div class="confirmation__requestNumber">
+      <h3 class="sub tw-mb-6">
+        {{ $t('checkout.requestNumber') }}
+      </h3>
+      <h3 class="tw-mb-6">1234567890</h3>
+      <h6 class="sub">
+        {{ $t('checkout.requestSupport') }}
+      </h6>
+    </div>
+    <div class="tw-col-span-12 tw-mb-4 text-center">
       <Button
-        class="registration-success-page__button"
-        :label="
-          $t(
-            `registration.registrationSuccessPage.${
-              isLiteRegistration ? 'lite' : 'full'
-            }.buttonLabel`
-          )
-        "
+        variant="primary"
+        :to="localePath('shop-my-account-request-history') + '/' + '1234567890'"
         icon="arrow_forward"
+        :label="$t('checkout.viewRequest')"
+      />
+    </div>
+    <div class="tw-col-span-12 tw-text-center">
+      <Button
         variant="secondary"
-        :href="localePath('/')"
+        class="back"
+        :to="localePath('shop')"
+        icon="arrow_forward"
+        :label="$t('checkout.backToHomepage')"
       />
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, useRoute, computed } from '@nuxtjs/composition-api'
-import Icon from '~/components/atoms/Icon/Icon'
+import { defineComponent } from '@nuxtjs/composition-api'
 import Button from '~/components/atoms/Button/Button'
 
 export default defineComponent({
   name: 'Confirmation',
-  components: { Icon, Button },
-  setup() {
-    const route = useRoute()
+  components: { Button },
+  props: {
+    order: {
+      type: Object,
+      required: true,
+    },
+  },
+  mounted() {
+    if (this.order) {
+      this.prepareDataLayer()
+    }
+  },
+  methods: {
+    prepareDataLayer() {
+      const totalPrice = this.order.totalPrice.value.toFixed(2)
+      const ecommerceItems = []
+      for (const entry of this.order.entries) {
+        const product = entry.product
+        const productCategories = product.categoryPath
+        const basePrice = entry?.basePrice?.value
 
-    const isLiteRegistration = computed(() => {
-      return route.value.query?.type === 'lite'
-    })
+        const tmpObj = {
+          item_name: product.name,
+          item_id: product.code,
+          price: basePrice ? basePrice.toFixed(2) : null,
+          item_brand: null,
+          item_category: productCategories?.[0]?.id || null,
+          item_category_2: productCategories?.[1]?.id || null,
+          item_category_3: productCategories?.[2]?.id || null,
+          item_category_4: productCategories?.[3]?.id || null,
+          quantity: entry.quantity,
+        }
 
-    return { isLiteRegistration }
+        ecommerceItems.push(tmpObj)
+      }
+
+      const dataLayerData = {
+        event: 'purchase',
+        ecommerce: {
+          purchase: {
+            transaction_id: this.order.code, // ID of the transaction / quote
+            value: totalPrice.toString(), // Total value of the transaction / quote
+            currency: totalPrice.currencyIso, // Currency
+            items: ecommerceItems,
+          },
+        },
+      }
+      /* eslint-enable */
+
+      this.$store.dispatch('pushToDataLayer', dataLayerData)
+    },
   },
 })
 </script>
 
-<style lang="scss">
-.registration-success-page {
-  @apply tw-flex;
-  @apply tw-justify-center;
-  @apply tw-h-full;
+<style lang="scss" scoped>
+.confirmation {
+  @apply tw-grid tw-grid-cols-12;
+  @apply tw-text-center;
 
-  &--content-wrapper {
-    @apply tw-flex tw-flex-col;
-    @apply tw-items-center;
-    max-width: 745px;
+  &__headline {
+    @apply tw-col-span-12;
+    @apply tw-mb-6;
   }
 
-  &__icon {
-    @apply tw-text-pv-green;
-    @apply tw-mb-4;
-    font-size: 33.33px !important;
-
-    @screen md {
-      @apply tw-mb-9;
-      font-size: 53.33px !important;
-    }
-
-    @screen lg {
-      @apply tw-mb-14;
-      font-size: 66.67px !important;
-    }
+  &__description {
+    @apply tw-col-span-12;
+    @apply tw-mb-12;
   }
 
-  &__title {
-    @apply tw-text-center;
-    @apply tw-mb-2;
-  }
-
-  &__subline {
-    @apply tw-text-center;
-    @apply tw-mb-1;
-
-    @screen md {
-      @apply tw-mb-6;
-    }
-
-    @screen lg {
-      @apply tw-mb-8;
-    }
-  }
-
-  &__paragraph {
-    @apply tw-text-base;
-    @apply tw-text-center;
-    @apply tw-mb-8;
-
-    @screen lg {
-      @apply tw-mb-12;
-      @apply tw-text-xl;
-    }
-  }
-
-  &__button {
-    @apply tw-self-stretch;
-
-    @screen md {
-      @apply tw-self-center;
-    }
+  &__requestNumber {
+    @apply tw-col-span-12;
+    @apply tw-mb-12;
   }
 }
 </style>
