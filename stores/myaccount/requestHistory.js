ce99693a989e102f50978d85eac13af9eb0db966
Jessica Holst <>
2023-02-27 09:36:56 +0100

    PVWEB-487 | feat | requestHistory store

--- stores/myaccount/requestHistory.js
+++ stores/myaccount/requestHistory.js
@@ -1,23 +1,33 @@
-import { defineStore } from 'pinia'
+import { defineStore, ref } from 'pinia'
 import { useAxiosForHybris } from '~/composables/useAxiosForHybris'
 
 export const useRequestHistoryStore = defineStore('requestHistory', () => {
   const { axios } = useAxiosForHybris()
 
-  const loadCountries = async () => {
-    const result = await axios.$get(config.ORDER_API, {}).catch((error) => {
-      logger.error(
-        'Error when fetching countries. Returning empty array.',
-        error
-      )
-    })
+  const loadRequestHistory = async (page, size = 5) => {
+    try {
+      const result = await axios.$get(config.ORDER_API, {
+        params: {
+          fields: 'FULL',
+          pageSize: size,
+          currentPage: page - 1 || 0,
+        },
+      })
 
-    countries.value = result?.countries || []
+      if (result && Array.isArray(result.orders) && !result.error) {
+        return result
+      }
+    } catch (e) {
+      logger.error(
+        'Error when getting orders. Returning empty array.',
+        e ? e : ''
+      )
+      return []
+    }
   }
 
   const requestHistory = ref([])
 
-  const loadRequestHistory = () => {}
   return {
     // States
     requestHistory,
