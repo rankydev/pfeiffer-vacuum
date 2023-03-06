8ed40697fe0e894676d650b28fd06a04053ea9c4
dhofmann <daniela.hofmann@diva-e.com>
2023-03-06 11:53:37 +0100

    PVWEB-539 | feat | add components CalculationList.vue and CalculationListItem.vue

---
+++ components/organisms/CalculationList/CalculationList.stories.js
@@ -0,0 +1,40 @@
+import CalculationList from './CalculationList.vue'
+import { lists, selectMode } from './CalculationList.stories.content.js'
+
+const argTypes = {
+  lists,
+  selectMode,
+}
+
+export default {
+  title: 'Molecules/o_120 Calculation List',
+  component: CalculationList,
+  parameters: {
+    docs: {
+      description: {
+        component: 'Calculation list table for calculations overview page',
+      },
+      source: {
+        code: '<CalculationList v-bind="args" />',
+      },
+    },
+  },
+  argTypes,
+}
+
+const Template = (args) => ({
+  components: {
+    CalculationList,
+  },
+  setup() {
+    return { args }
+  },
+  template: `
+  <div class="documentation-preview">
+    <CalculationList v-bind="args" />
+  </div>
+`,
+})
+
+export const Default = Template.bind({})
+Default.args = argTypes
