b5d74dc83e5b9a638b439a55194fc55ce854621a
David Gonzalez Casin <david.gonzalezcasin@diva-e.com>
2023-03-08 15:53:10 +0100

    PVWEB-903 | feat | Refactoring of oci middleware

--- server/middleware/oci.js
+++ server/middleware/oci.js
@@ -4,24 +4,23 @@ import {
   OCI_HOOK_URL,
   OCI_RETURN_TARGET,
   OCI_CUSTOMER_ID,
+  OCI_BUYERCOOKIE,
 } from '../constants.js'
 
 const express = require('express')
 const app = express()
 app.set('x-powered-by', false)
 
-// extracts parameter (name) from url string
-function getParameterByName(name, url) {
-  name = name.replace(/[[\]]/g, '\\$&')
-  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
+function getBuyercookieFromHookUrl(url) {
+  const regex = new RegExp('[?&]' + OCI_BUYERCOOKIE + '(=([^&#]*)|&|#|$)')
   const results = regex.exec(url)
 
-  if (!results) {
+  // checks if any results were found and returns null if not
+  // results[2] contains the value of the query parameter
+  if (!results || !results[2]) {
     return null
   }
-  if (!results[2]) {
-    return ''
-  }
+
   return decodeURIComponent(results[2].replace(/\+/g, ' '))
 }
 
@@ -31,8 +30,9 @@ app.post('/oci', (req, res) => {
   const password = req.body[OCI_PASSWORD]
   const hookUrl = req.body[OCI_HOOK_URL]
 
-  // uni Lund does not send customerId it will send buyercookie
-  const buyercookie = getParameterByName('buyercookie', hookUrl)
+  // uni Lund does not send customerId, it will send buyercookie
+  // which comes as a query parameter in the hook url
+  const buyercookie = getBuyercookieFromHookUrl(hookUrl)
   const customerId = buyercookie || req.body[OCI_CUSTOMER_ID]
 
   const returntarget = req.body[OCI_RETURN_TARGET]
