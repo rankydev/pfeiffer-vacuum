05879b98c7a4c4f9022750ac71e7a7c81da7a385
Nico <nico.hofmair@gmail.com>
2023-03-13 21:18:34 +0100

    PVWEB-815 | feat | only scroll top on real path change an on pagination changes

--- app/router.scrollBehavior.js
+++ app/router.scrollBehavior.js
@@ -1,10 +1,14 @@
-export default function (to) {
-  // Scroll to hash if set and on same site.
+export default function (to, from) {
   if (to.hash !== '' && document !== null && document.querySelector(to.hash)) {
+    // Scroll to hash if set and on same site.
     document.querySelector(to.hash).scrollIntoView({ behavior: 'smooth' })
     return
   } else {
-    // TODO: This let's us scroll to the top on every page, check if this is always intended
-    return { x: 0, y: 0 }
+    const pathChanged = from.path !== to.path
+    const paginationChanged = from.query?.currentPage !== to.query?.currentPage
+
+    if (pathChanged || paginationChanged) {
+      return { x: 0, y: 0 }
+    }
   }
 }
