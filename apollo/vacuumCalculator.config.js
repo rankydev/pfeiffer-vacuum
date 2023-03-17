6aa60441a62cb237db454da729690f6a7c92449a
David Gonzalez Casin <david.gonzalezcasin@diva-e.com>
2023-03-17 17:56:21 +0100

    PVWEB-540 | feat | Implemented authorization for GraphQL

--- apollo/vacuumCalculator.config.js
+++ apollo/vacuumCalculator.config.js
@@ -1,25 +1,34 @@
-import { useUserStore } from '~/stores/user'
+import { setContext } from 'apollo-link-context'
 import { storeToRefs } from 'pinia'
+import { useUserStore } from '~/stores/user'
+import { PATH_VACUUM_CALC } from '~/server/constants.js'
 
-export default () => {
+export default (context) => {
+  const { $config } = context
+  const { CURRENT_REGION_CODE, baseURL } = $config
+
+  const authLink = setContext(async (_, { headers }) => {
+    const userStore = useUserStore()
+    const { auth } = storeToRefs(userStore)
+    const authHeader =
+      $config.NODE_ENV === 'development' ? 'authorization-gql' : 'Authorization'
+    return {
+      headers: {
+        ...headers,
+        [authHeader]: `${auth.value?.token_type} ${auth.value?.access_token}`,
+      },
+    }
+  })
+
   return {
-    // TODO: FIX ENDPOINT
-    httpEndpoint: 'https://dev2.pvac.diva-e.com',
-    browserHttpEndpoint: '/api/vacuumCalc/graphql',
+    httpEndpoint: baseURL,
+    browserHttpEndpoint: `/${CURRENT_REGION_CODE}${PATH_VACUUM_CALC}/graphql`,
     httpLinkOptions: {
       headers: {
-        // TODO DYNAMIC CURRENT LANG
-        'Accept-Language': 'en',
+        // TODO this doesn't work right now because somehow i18n is not available at that point
+        'Accept-Language': context.app?.i18n?.locale === 'de' ? 'de' : 'en',
       },
     },
-    getAuth: () => {
-      const userStore = useUserStore()
-      const { auth } = storeToRefs(userStore)
-
-      // eslint-disable-next-line camelcase
-      return auth.value
-        ? `${auth.value?.token_type} ${auth.value?.access_token}`
-        : null
-    },
+    link: authLink,
   }
 }
